c071c1cb9cd4799b596ff50f1abb04204f1ccea2
zerodayz <cerninr@gmail.com>
2017-06-11 13:44:00 -0400

    Added support for python modules.

---
+++ osp-director.d/hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding=utf-8 (pep 0263)
+
+""" docstring """
+
+# imports
+import sys
+# functions
+
+if __name__ == "__main__":
+    # actual main
+    DIRECTORY = sys.argv[1]
+
+    print "+--------------------------------------------+"
+    print "|             Python Hello Module            |"
+    print "+--------------------------------------------+"
+    print "Hello from python module!"
+    print "Working directory is ", DIRECTORY
+
