228e43834add5cf898046754d0d901e81421bbb2
Robin Cernin <rcernin@collab-shell.usersys.redhat.com>
2017-06-14 03:59:37 -0400

    Adding kernel module.

---
+++ generic.d/kernel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright (C) 2017   Robin Cernin (rcernin@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Checking Tracebacks
+# Ref: None
+
+echo "+--------------------------------------------+"
+echo "|                Kernel Module               |"
+echo "+--------------------------------------------+"
+
+
+# Looks for the Kernel Out of Memory, panics and soft locks
+
+grep_file_rev "${DIRECTORY}/sos_commands/logs/journalctl_--no-pager_--boot" "oom-killer"
+grep_file_rev "${DIRECTORY}/sos_commands/logs/journalctl_--no-pager_--boot" "soft lockup"
