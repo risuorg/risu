53657de0a7634bd4e11b3c4b1d2ae4545e3d7ebe
Robin Cernin <cerninr@gmail.com>
2017-07-17 17:11:49 -0400

    Removed check on ClusterHA for non-HA nodes

--- plugins/openstack/clusterha.sh
+++ plugins/openstack/clusterha.sh
@@ -88,53 +88,57 @@ function clusterha_check_sosreport(){
   # Checking the number of nodes in the cluster.
   # Check which directory for cluster exists it's either cluster or pacemaker
 
-  for CLUSTER_DIRECTORY in "pacemaker" "cluster"; do
-
-    if [ -d "${DIRECTORY}/sos_commands/${CLUSTER_DIRECTORY}" ]
-    then
-      PCS_DIRECTORY="${DIRECTORY}/sos_commands/${CLUSTER_DIRECTORY}"
-    fi
-  done
-
-  if [ -z "${PCS_DIRECTORY}" ]
+  if grep -q "pacemaker.*active" "${DIRECTORY}/sos_commands/systemd/systemctl_list-units_--all"
   then
-    continue
-  else
+    for CLUSTER_DIRECTORY in "pacemaker" "cluster"; do
 
-    NUM_NODES=$(sed -n -r -e 's/^([0-9])[ \t]+nodes.*/\1/p' "${PCS_DIRECTORY}/pcs_status")
-    if [ "$echo $(( (NUM_NODES-1) % 2 ))" -eq  "0" ]
-    then
-      good "The nodes in cluster are equal to ${NUM_NODES}."
-    else
-      bad "There are ${NUM_NODES} in cluster."
-    fi
-
-    # Checking for stonith-enabled: true
-    grep_file "${PCS_DIRECTORY}/pcs_config" "stonith-enabled:.*true"
-
-    # Checking if there are any "Failed Actions" in the pcs_status
-    grep_file_rev "${PCS_DIRECTORY}/pcs_status" "Failed Actions"
-
-    # Checking if there are any "Stopped" services
-    grep_file_rev "${PCS_DIRECTORY}/pcs_status" "Stopped"
-
-    # Check packages version
-    PCS_VERSION=$(sed -n -r -e 's/^pacemaker.*-1.1.([0-9]+)-.*$/\1/p' "${DIRECTORY}/installed-rpms")
-    for package in ${PCS_VERSION}
-    do
-      if [[ "${package}" -lt "15" ]]
+      if [ -d "${DIRECTORY}/sos_commands/${CLUSTER_DIRECTORY}" ]
       then
-	VERSION_CHECK="1"
+	PCS_DIRECTORY="${DIRECTORY}/sos_commands/${CLUSTER_DIRECTORY}"
       fi
     done
-    if [[ "${VERSION_CHECK}" -eq "1" ]]
-    then
-      bad "Pacemaker packages are older than 1.1.15."
-    else
-      good "Pacemaker version is greater than or equal to 1.1.15."
-    fi
-  fi
 
+    if [ -z "${PCS_DIRECTORY}" ]
+    then
+      continue
+    else
+
+      NUM_NODES=$(sed -n -r -e 's/^([0-9])[ \t]+nodes.*/\1/p' "${PCS_DIRECTORY}/pcs_status")
+      if [ "$echo $(( (NUM_NODES-1) % 2 ))" -eq  "0" ]
+      then
+	good "The nodes in cluster are equal to ${NUM_NODES}."
+      else
+	bad "There are ${NUM_NODES} in cluster."
+      fi
+
+      # Checking for stonith-enabled: true
+      grep_file "${PCS_DIRECTORY}/pcs_config" "stonith-enabled:.*true"
+
+      # Checking if there are any "Failed Actions" in the pcs_status
+      grep_file_rev "${PCS_DIRECTORY}/pcs_status" "Failed Actions"
+
+      # Checking if there are any "Stopped" services
+      grep_file_rev "${PCS_DIRECTORY}/pcs_status" "Stopped"
+
+      # Check packages version
+      PCS_VERSION=$(sed -n -r -e 's/^pacemaker.*-1.1.([0-9]+)-.*$/\1/p' "${DIRECTORY}/installed-rpms")
+      for package in ${PCS_VERSION}
+      do
+	if [[ "${package}" -lt "15" ]]
+	then
+	  VERSION_CHECK="1"
+	fi
+      done
+      if [[ "${VERSION_CHECK}" -eq "1" ]]
+      then
+	bad "Pacemaker packages are older than 1.1.15."
+      else
+	good "Pacemaker version is greater than or equal to 1.1.15."
+      fi
+    fi
+  else
+    continue
+  fi
 
 }
 
