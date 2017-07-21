4dbf3a88f3471175e42788e0384829d571c63a40
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-07-21 20:38:33 +0200

    Add support for tox, pbr, etc

---
+++ tests/test_basic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from unittest import TestCase
+import citellus
+
+
+class CitellusTest(TestCase):
+    def test_switch(self):
+        self.assertEqual('1', '1')
