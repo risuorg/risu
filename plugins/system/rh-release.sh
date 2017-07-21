a175646178fca8a8498ce0bed3c791112e930960
Robin Cernin <cerninr@gmail.com>
2017-07-21 02:50:52 -0400

    Add Red Hat release version check.

---
+++ plugins/system/rh-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright (C) 2017   Robin Cernin (rcernin@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# check redhat-release
+
+if [ ! -f "${CITELLUS_ROOT}/etc/redhat-release" ]; then
+  echo "file /etc/redhat-release not found." >&2
+  exit 2
+fi
+
+if grep -q "^Red Hat.*7\.3.*$" "${CITELLUS_ROOT}/etc/redhat-release"
+then
+  exit 0
+else
+  cat /etc/redhat-release >&2
+  exit 1
+fi
