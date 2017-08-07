84672c7cdbbac62c26760dabd80e72fa29e60e20
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-08-07 14:03:20 +0200

    Fix pep8 again (tox -e flake8)
    Change-Id: Ibc9373f5800a38f6430ee71e3a6e3da04d015cab

--- citellus/citellus.py
+++ citellus/citellus.py
@@ -209,7 +209,7 @@ def formattext(returncode):
 
 def indent(text, amount):
     padding = ' ' * amount
-    return '\n'.join(padding+line for line in text.splitlines())
+    return '\n'.join(padding + line for line in text.splitlines())
 
 
 def parse_args():
@@ -354,10 +354,10 @@ def main():
         print("# %s: %s" % (result['plugin'], text))
 
         show_err = (
-            (rc in [RC_FAILED])
-            or (rc not in [RC_OKAY, RC_FAILED, RC_SKIPPED])
-            or (rc in [RC_SKIPPED] and options.verbose > 0)
-            or (options.verbose > 1)
+            (rc in [RC_FAILED]) or
+            (rc not in [RC_OKAY, RC_FAILED, RC_SKIPPED]) or
+            (rc in [RC_SKIPPED] and options.verbose > 0) or
+            (options.verbose > 1)
         )
 
         show_out = (
@@ -369,6 +369,7 @@ def main():
 
         if show_err and err.strip():
             print(indent(err, 4))
+
 
 if __name__ == "__main__":
     main()
