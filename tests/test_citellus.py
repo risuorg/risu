62f75130d931f710d7aecb761f8e46ae6035389b
Lars Kellogg-Stedman <lars@redhat.com>
2017-08-07 10:57:05 -0400

    tests: cleanups and more tests
    this commit improves citellus testing:
    
    - reduces number of required modules in test-requirements.txt
    - moves from ostestr to pytest for driving tests
    - implements tests for most operational methods in citellus.py
    - renames 'flake8' test to 'pep8' to match common openstack usage
    - fixes all outstanding pep8 issues so that tests pass correctly
    - disables "markdown" test, because none of the markdown files pass it
      at the moment.
    
    Change-Id: Ic801388c9081be9cea2b7f7deac8c6c4337f9f9c

---
+++ tests/test_citellus.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+import os
+from unittest import TestCase
+from citellus import citellus
+
+testplugins = os.path.join(citellus.citellusdir, 'testplugins')
+
+
+class CitellusTest(TestCase):
+    def test_runplugin_pass(self):
+        res = citellus.runplugin(os.path.join(testplugins, 'exit_passed.sh'))
+        assert res['result']['rc'] == 0
+        assert res['result']['out'].endswith('something on stdout\n')
+        assert res['result']['err'].endswith('something on stderr\n')
+
+    def test_runplugin_fail(self):
+        res = citellus.runplugin(os.path.join(testplugins, 'exit_failed.sh'))
+        assert res['result']['rc'] == 1
+        assert res['result']['out'].endswith('something on stdout\n')
+        assert res['result']['err'].endswith('something on stderr\n')
+
+    def test_runplugin_skip(self):
+        res = citellus.runplugin(os.path.join(testplugins, 'exit_skipped.sh'))
+        assert res['result']['rc'] == 2
+        assert res['result']['out'].endswith('something on stdout\n')
+        assert res['result']['err'].endswith('something on stderr\n')
+
+    def test_findplugins_positive_filter_include(self):
+        plugins = citellus.findplugins([testplugins],
+                                       include=['exit_passed'])
+
+        assert len(plugins) == 1
+
+    def test_findplugins_positive_filter_exclude(self):
+        plugins = citellus.findplugins([testplugins],
+                                       exclude=['exit_passed'])
+
+        for plugin in plugins:
+            assert 'exit_passed' not in plugin
+
+    def test_findplugins_positive(self):
+        assert len(citellus.findplugins([testplugins])) != 0
+
+    def test_findplugins_negative(self):
+        assert citellus.findplugins('__does_not_exist__') == []
+
+    def test_docitellus(self):
+        plugins = citellus.findplugins([testplugins],
+                                       include=['exit_passed'])
+        results = citellus.docitellus(plugins=plugins)
+        assert len(results) == 1
+        assert results[0]['result']['rc'] == 0
