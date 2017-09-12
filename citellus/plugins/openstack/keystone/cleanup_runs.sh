23ce8f6ade5a4cf8408b33dc2c0f75ccdb22c787
Robin Cernin <cerninr@gmail.com>
2017-09-12 12:59:52 +0200

    Add check for controllers to cleanup_runs.sh
    Change-Id: I314bbecd1f2d7a3254fe46eff72c2c22cfdcc14b

--- citellus/plugins/openstack/keystone/cleanup_runs.sh
+++ citellus/plugins/openstack/keystone/cleanup_runs.sh
@@ -17,6 +17,19 @@
 
 # this can run against live and also fs snapshot
 
+if [ "x$CITELLUS_LIVE" = "x0" ];  then
+  if grep -q nova-compute "${CITELLUS_ROOT}/ps";
+  then
+    echo "works only on controller node" >&2
+    exit $RC_SKIPPED
+  fi
+elif [ "x$CITELLUS_LIVE" = "x1" ];  then
+  if ps -elf | grep -q [n]ova-compute; then
+    echo "works only on controller node" >&2
+    exit $RC_SKIPPED
+  fi
+fi
+
 if [ ! -f "${CITELLUS_ROOT}/var/log/keystone/keystone.log" ]; then
   echo "file /var/log/keystone/keystone.log not found." >&2
   exit $RC_SKIPPED
