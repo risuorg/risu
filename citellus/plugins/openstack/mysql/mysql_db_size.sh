ee22b2f5ac28ed4a7ddcbb3c6f8c1358e1899a93
Pablo Caruana <pcaruana@redhat.com>
2017-09-28 15:27:42 +0200

    [plugins][openstack][mysql] Check mysql db sizes `Closes #$107` corrected patch2 removed extra spaces and moved line 31 to 36 Change-Id: I92bd0a31964c2456530dedcf51f34a9c1a779349

---
+++ citellus/plugins/openstack/mysql/mysql_db_size.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Copyright (C) 2017   Pablo Caruana (pcaruana@redhat.com | pablo.caruana@gmail.com)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# this can run against live
+
+if [ ! "x$CITELLUS_LIVE" = "x1" ]; then
+  echo "works on live-system only" >&2
+  exit $RC_SKIPPED
+fi
+
+# This test requires mysql
+
+MYSQL_DIR="/var/lib/mysql"
+which mysql > /dev/null 2>&1
+RC=$?
+
+if [ "x$RC" = "x0" ];
+then
+    MYSQL_DB_SIZE=$(mysql -u root -e 'SELECT table_schema "DB Name", Round(Sum(data_length + index_length) / 1024 / 1024, 1) "DB Size in MB" FROM information_schema.tables GROUP BY table_schema;')
+    RC=$?
+    if [ "x$RC" = "x0" ];
+    then
+		echo "DB SCHEMA SIZE" >&2	
+		echo $MYSQL_DB_SIZE >&2
+    else
+        echo "no connection to the database" >&2
+        exit $RC_SKIPPED
+    fi
+else
+    echo "missing mysql binaries" >&2
+    exit $RC_SKIPPED
+fi
+
+if [ "$(ls -A $MYSQL_DIR)" ];
+then
+    MYSQL_DISK_USAGE=$(du -mshc $MYSQL_DIR/*)
+    echo "DB DISK USAGE details" >&2
+    echo $MYSQL_DISK_USAGE >&2
+    exit $RC_OKAY
+else
+    echo "$MYSQL_DIR doesn't exists" >&2
+    exit $RC_FAILED
+fi
