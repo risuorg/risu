e450bab2e3bd5c2a9d6c81345094a98d93830a32
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-09-28 11:38:08 +0200

    [citellus][plugins][bugzilla] BZ 1483456: Check if host might get issues updating ceilometer
    Closes #141
    
    Change-Id: I059a4dedb9429a4d7e60ff9f0de670b930b752d5

---
+++ citellus/plugins/bugzilla/ceilometer_bug_1483456.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright (C) 2017   Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# this can run against live and also any sort of snapshot of the filesystem
+
+# Load common functions
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
+
+is_required_file "${CITELLUS_ROOT}/etc/nova/nova.conf"
+
+if [ "x$CITELLUS_LIVE" = "x1" ];  then
+    HOST=$(hostname)
+elif [ "x$CITELLUS_LIVE" = "x0" ];  then
+  is_required_file "${CITELLUS_ROOT}/hostname"
+  HOST=$(cat "${CITELLUS_ROOT}/hostname")
+fi
+
+NOVAHOST=$(grep ^host.* "${CITELLUS_ROOT}/etc/nova/nova.conf"|cut -d "=" -f2|tail -1)
+
+if [[ "$HOST" != "$NOVAHOST" ]]; then
+  echo "https://bugzilla.redhat.com/show_bug.cgi?id=1483456" >&2
+  exit $RC_FAILED
+else
+  exit $RC_OKAY
+fi
