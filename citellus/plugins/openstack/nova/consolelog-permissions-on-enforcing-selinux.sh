63b7a4c7bf21adca6e14b76c26198b7a5ed1eb34
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-10-26 16:03:43 +0200

    [citellus][plugins][openstack][nova] Console.log fails to be removed on repeated migration or compute sudden restart
    Closes #170
    
    Change-Id: Ibc081b015c30ee47c98c7cf8a4526bee50637c2a

---
+++ citellus/plugins/openstack/nova/consolelog-permissions-on-enforcing-selinux.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright (C) 2017 Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Load common functions
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
+
+# check if we are running against compute
+
+is_process nova-compute || ( echo "works only on compute node" >&2 && exit $RC_SKIPPED )
+
+MESSAGE=$"AVC denial for console.log: https://bugzilla.redhat.com/show_bug.cgi?id=1501957 https://bugzilla.redhat.com/show_bug.cgi?id=1491767"
+
+if [ "x$CITELLUS_LIVE" = "x0" ];  then
+    if [ -z "${journalctl_file}" ]; then
+        echo "file /sos_commands/logs/journalctl_--no-pager_--boot not found." >&2
+        echo "file /sos_commands/logs/journalctl_--all_--this-boot_--no-pager not found." >&2
+        exit $RC_SKIPPED
+    fi
+    is_lineinfile '.*avc:.*denied.*unlink.*virtlogd.*name="console.log".*' ${journalctl_file} && echo "$MESSAGE" >&2 && exit $RC_FAILED
+elif [ "x$CITELLUS_LIVE" = "x1" ]; then
+    if journalctl --no-pager --boot | grep -qe '.*avc:.*denied.*unlink.*virtlogd.*name="console.log".*'; then
+        echo "$MESSAGE" >&2
+        exit $RC_FAILED
+    fi
+fi
+
+exit $RC_OKAY
