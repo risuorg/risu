613478d6365194a5580bc086c9ce9f4a21ff5f79
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-11-07 17:05:04 +0100

    [citellus][plugins][openstack][dpdk] Check CPU overlapping between options
    Closes #182
    
    Change-Id: I47de179f0fa2ec74027e459cc39c2eec3f94fbc8

--- citellus/plugins/openstack/network/dpdk.sh
+++ citellus/plugins/openstack/network/dpdk.sh
@@ -22,6 +22,16 @@
 # Load common functions
 [ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
 
+expand_ranges(){
+    for CPU in $(echo $*|tr "," "\n");do
+        if [[ "$CPU" == *"-"* ]];then
+            echo $CPU|awk -F "-" '{print $1" "$2}'|xargs seq
+        else
+             echo "$CPU"
+        fi
+    done
+}
+
 # Actual code execution
 flag=0
 
@@ -44,26 +54,214 @@ elif [ $CITELLUS_LIVE -eq 1 ];then
     ovs-vsctl -t 5 get Open_vSwitch . other_config > $FILE
 fi
 
-if is_lineinfile "dpdk-init.*true" "${FILE}";then
-    # DPDK is supposedly enabled, do further checks
+if [ -f "$FILE" ]; then
 
-    if ! is_lineinfile "dpdk-socket-mem=" "${FILE}";then
-        echo $"missing dpdk-socket-mem in ovs-vsctl" >&2
-        flag=1
+    if is_lineinfile "dpdk-init.*true" "${FILE}";then
+        # DPDK is supposedly enabled, do further checks
+
+        if ! is_lineinfile "dpdk-socket-mem=" "${FILE}";then
+            echo $"missing dpdk-socket-mem in ovs-vsctl" >&2
+            flag=1
+        fi
+        if ! is_lineinfile "dpdk-lcore-mask=" "${FILE}";then
+            echo $"missing dpdk-lcore-mask= (Core list) in ovs-vsctl" >&2
+            flag=1
+        fi
+        if ! is_lineinfile "pmd-cpu-mask=" "${FILE}";then
+            echo $"missing pmd-cpu-mask= (pmd cpu mask) in ovs-vsctl" >&2
+            flag=1
+        fi
     fi
-    if ! is_lineinfile "dpdk-lcore-mask=" "${FILE}";then
-        echo $"missing dpdk-lcore-mask= (Core list) in ovs-vsctl" >&2
-        flag=1
+
+    procids=$(grep ^processor ${CITELLUS_ROOT}/proc/cpuinfo|cut -d ":" -f 2|xargs echo)
+
+    if ! is_lineinfile "isolcpus" "${CITELLUS_ROOT}/proc/cmdline"; then
+        # Check Systemd as alternative:
+        # The only step required is hence to configure the CPUAffinity option in /etc/systemd/system.conf.
+        # Systemd CPUAffinity should be 'negative' of ISOLCPU's so need to get all CPU's and reverse
+        systemdaffinity=$(grep CPUAffinity ${CITELLUS_ROOT}/etc/systemd/system.conf|cut -d "=" -f 2)
+        systemdaffinity=$(expand_ranges $systemdaffinity)
+
+        # Loop for getting reversed array (items not in)
+        isolated=""
+        for i in ${procids[@]}; do
+            present=0
+            for j in ${systemdaffinity[@]}; do
+                if [ $i -eq $j ];then
+                    present=1
+                fi
+            done
+            if [ $present -eq 0 ];then
+                isolated="$isolated $i"
+            fi
+        done
+        ISOLCPUS=$isolated
+    elif is_lineinfile isolcpus ${CITELLUS_ROOT}/proc/cmdline; then
+        ISOLCPUS=$(cat ${CITELLUS_ROOT}/proc/cmdline|tr " " "\n"|grep ^isolcpus|cut -d "=" -f 2-|tr "," "\n")
+        ISOLCPUS=$(expand_ranges $ISOLCPUS)
+    else
+        unset ISOLCPUS
     fi
-    if ! is_lineinfile "pmd-cpu-mask=" "${FILE}";then
-        echo $"missing pmd-cpu-mask= (pmd cpu mask) in ovs-vsctl" >&2
+
+    USEDBYKERNEL=""
+    # Check CPU's not isolated:
+    for i in ${procids[@]}; do
+        present=1
+        for j in ${ISOLCPUS[@]}; do
+            if [ $i -eq $j ];then
+                present=0
+            fi
+        done
+        if [ $present -eq 1 ];then
+            USEDBYKERNEL="$USEDBYKERNEL $i"
+        fi
+
+    done
+
+    echo "KERNEL used CPU's: $USEDBYKERNEL" >&2
+
+    FREECPUS=$(echo "$ISOLCPUS"|xargs echo)
+    echo "CPU's isolated from kernel scheduler $FREECPUS" >&2
+
+    # List of CPU's that other components can use
+    VCPUPINSET=$(grep vcpu_pin_set ${CITELLUS_ROOT}/etc/nova/nova.conf|cut -d "=" -f 2-|tr "," "\n")
+    VCPUPINSET=$(expand_ranges $VCPUPINSET)
+
+    USEDBYNOVA=""
+    # Check that NOVA is configured for using CPU's in isolated
+    for i in ${VCPUPINSET[@]}; do
+        present=0
+        for j in ${ISOLCPUS[@]}; do
+            if [ $i -eq $j ];then
+                present=1
+                USEDBYNOVA="$USEDBYNOVA $i"
+            fi
+        done
+        if [ $present -eq 0 ];then
+            echo -n $"Nova VCPU in vcpu_pin_set not in Isolated CPU's " >&2
+            echo "CPU $i" >&2
+            flag=1
+        fi
+    done
+
+    echo "Nova used CPU's: $USEDBYNOVA" >&2
+
+    # Update the actual list of 'Free' CPU's
+    NEWFREE=""
+    for i in ${FREECPUS[@]}; do
+        present=0
+        for j in ${USEDBYNOVA[@]}; do
+            if [ $i -eq $j ];then
+                present=1
+            fi
+        done
+        if [ $present -eq 0 ];then
+            NEWFREE="$NEWFREE $i"
+        fi
+    done
+
+    FREECPUS="$NEWFREE"
+
+    # So, first processor (0) is assigned, then 5 unused ones, and next one (6) is enabled, then 5 more unused,
+    # then next one is enabled (12), then 5 unused, then one enabled (18), being H the highest processor count
+    # and L the lowest (0)
+
+    # The pmd-cpu-mask is 082082, meaning:
+    # 1000 0010 0000 1000 0010
+    # CPU 1, CPU 7, CPU 13, CPU 19
+
+    DPDKPMDMASK=$(cat $FILE|tr " {}," "\n"|grep "^pmd-cpu-mask"|cut -d "=" -f 2|tr -d '"')
+    BINPMDMASK=$(echo "obase=2; ibase=16; $DPDKPMDMASK" | bc)
+
+    # Walk the binary PMD mask to find processor numbers
+    PMDCPUS=""
+    foo="$BINPMDMASK"
+    j=0
+    for (( i=${#foo}-1; i>=0; i-- )); do
+        enabled=0
+        enabled="${foo:$i:1}"
+        if [ "$enabled" = "1" ];then
+            PMDCPUS="$PMDCPUS $j"
+        fi
+        j=$((j+1))
+    done
+
+    echo "DPDK PMD used CPU's: $PMDCPUS" >&2
+
+
+    # Remove from FREE CPU's the ones used by DPDK PMD
+    NEWFREE=""
+    for i in ${FREECPUS[@]}; do
+        present=0
+        for j in ${PMDCPUS[@]}; do
+            if [ $i -eq $j ];then
+                present=1
+            fi
+        done
+        if [ $present -eq 0 ];then
+            NEWFREE="$NEWFREE $i"
+            echo -n $"DPDK PMD CPU was already used by Nova " >&2
+            echo "CPU $i" >&2
+            flag=1
+        fi
+    done
+
+    # cat overcloud-compute-0/sos_commands/openvswitch/ovs-vsctl_-t_5_get_Open_vSwitch_._other_config
+    # {dpdk-init="true", dpdk-lcore-mask="41041", dpdk-socket-mem="2048,2048", pmd-cpu-mask="082082"}
+
+    # Mask provided is 00041041 hex, which translates to binary:
+
+    # H rL
+    # 1000001000001000001
+    # CPU 0, CPU 6, CPU 12, CPU 18
+
+    # We'll be using bc for the conversion:
+
+    DPDKCOREMASK=$(cat $FILE|tr " {}," "\n"|grep "^dpdk-lcore-mask"|cut -d "=" -f 2|tr -d '"')
+    BINCOREMASK=$(echo "obase=2; ibase=16; $DPDKCOREMASK" | bc)
+
+    # Walk the binary CPU mask to find processor numbers
+    CORECPUS=""
+    foo="$BINCOREMASK"
+    j=0
+    for (( i=${#foo}-1; i>=0; i-- )); do
+        enabled=0
+        enabled="${foo:$i:1}"
+        if [ "$enabled" = "1" ];then
+            CORECPUS="$CORECPUS $j"
+        fi
+        j=$((j+1))
+    done
+
+    echo "DPDK CORE used CPU's: $CORECPUS" >&2
+
+    # Remove from FREE CPU's the ones used by DPDK COREMASK
+    NEWFREE=""
+    USEDCPUS="$PMDCPUS $USEDBYNOVA $USEDBYKERNEL"
+
+    for i in ${USEDBYKERNEL[@]}; do
+        present=0
+        for j in ${CORECPUS[@]}; do
+            if [ $i -eq $j ];then
+                present=1
+                echo -n $"DPDK CORE CPU was already used by Kernel, Nova or DPDK PMD " >&2
+                echo "CPU $i" >&2
+                # NOTE: DPDK CORE CPU's are not mandatory to be isolated, so don't flag
+                # flag=1
+            fi
+        done
+        if [ $present -eq 0 ];then
+            NEWFREE="$NEWFREE $i"
+        fi
+    done
+
+    FREECPUS="$NEWFREE"
+
+    if [ "${#FREECPUS}" != "0" ];then
+        echo "There are CPU's unallocated: $FREECPUS" >&2
         flag=1
     fi
 fi
-
-# TODO(iranzo)
-# ./sos_commands/openvswitch/ovs-vsctl_-t_5_show
-# type: dpdk
 
 # DPDK: NeutronBridgeMappings: 'dpdk:br-link'
 #  NeutronDpdkCoreList: "'4,6,20,22'"
