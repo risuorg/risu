c39fed66cc7dd6f65e83041ed55183fd855d126e
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-11-09 08:54:48 +0100

    [citellus][plugins][openstack][redis] Check for Tooz lock
    Closes #186
    
    Change-Id: I1b166b173c4a780203e4e534d360e7b8bcf6470c

---
+++ citellus/plugins/openstack/redis/redis-tooz-lock.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright (C) 2017   Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# we can run this against fs snapshot or live system
+
+# Load common functions
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
+
+is_required_file "${CITELLUS_ROOT}/var/log/gnocchi/metricd.log"
+
+if is_lineinfile "ToozError: Cannot extend an unlocked lock" "${CITELLUS_ROOT}/var/log/gnocchi/metricd.log"; then
+    echo $"tooz https://bugzilla.redhat.com/show_bug.cgi?id=1465385" >&2
+    exit $RC_FAILED
+fi
+exit $RC_OKAY
