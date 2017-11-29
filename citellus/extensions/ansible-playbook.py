9a0f4d8d02c8925ed71a2095912fbb4ea6fa72bd
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-11-29 12:29:30 +0100

    [citellus][core] Include support for extensions and ansible extension
    Closes #239
    
    Change-Id: I96239f3e307b7bc7eb81d7706a1c0d6850a71c83

---
+++ citellus/extensions/ansible-playbook.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Extension for processing ansible playbooks
+# Author: Pablo Iranzo Gomez (Pablo.Iranzo@gmail.com)
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for plugin
+    """
+    triggers = []
+    return triggers
+
+
+def run(message):  # do not edit this line
+    """
+    Executes plugin
+    :param message: message to run against
+    :return:
+    """
+
+    return
+
+
+def help(message):  # do not edit this line
+    """
+    Returns help for plugin
+    :param message: message to process
+    :return: help text
+    """
+
+    commandtext = ""
+    return commandtext
