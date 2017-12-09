18c214e5616a956f8b669c5a9d0a6f97cd756311
Robin Cernin <cerninr@gmail.com>
2017-12-09 23:42:07 +1000

    [citellus][plugins][mysql] Add check for clustercheck MYSQL_HOST
    Closes #265
    
    Change-Id: I3a93b04617fb38a8ce3032c9f03896b25d39d50a

---
+++ citellusclient/plugins/core/openstack/mysql/clustercheck-mysql_host.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright (C) 2017   Robin Černín (rcernin@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# we can run this against fs snapshot or live system
+
+# description: Checks clustercheck and it's variable $MYSQL_HOST is set to localhost
+
+# Load common functions
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if is_process nova-compute;then
+    echo "works only on controller node" >&2
+    exit $RC_SKIPPED
+fi
+
+is_required_file "${CITELLUS_ROOT}/etc/sysconfig/clustercheck"
+if is_lineinfile "^MYSQL_HOST[ \t]*=[ \t]*localhost$" "${CITELLUS_ROOT}/etc/sysconfig/clustercheck"; then
+    exit $RC_OKAY
+else
+    echo $"clustercheck variable MYSQL_HOST should be set to localhost." >&2
+    grep "^MYSQL_HOST" "${CITELLUS_ROOT}/etc/sysconfig/clustercheck" >&2
+    exit $RC_FAILED
+fi
