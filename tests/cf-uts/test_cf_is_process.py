ad48de057d1e26138d97e8e11835dad169638c06
Robin Cernin <cerninr@gmail.com>
2017-12-09 13:52:12 +1000

    [citellus] Moving citellus to citellusclient
    Change-Id: Ib5ba3b392d7ea86fbdfcd8c87939cff33b18be66

--- tests/cf-uts/test_cf_is_process.py
+++ tests/cf-uts/test_cf_is_process.py
@@ -24,7 +24,7 @@ from unittest import TestCase
 import tempfile
 import shutil
 
-from citellus import citellus
+import citellusclient.citellus as citellus
 
 # To create your own test, update NAME with plugin name and copy this file to test_$NAME.py
 NAME = "test_cf_is_process"
