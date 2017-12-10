2b227f543146b6019a0c3337746ab6701deb78fe
Robin Cernin <cerninr@gmail.com>
2017-12-10 20:19:15 +1000

    [citellus][plugins][pacemaker] Move number nodes from pcs status to pcs config
    Closes #289
    
    Change-Id: Ia4714a637e088eb9f727bcbd04f3c261940cf46c

--- citellusclient/plugins/core/pacemaker/nodes_number.sh
+++ citellusclient/plugins/core/pacemaker/nodes_number.sh
@@ -40,7 +40,7 @@ if ! is_active pacemaker; then
 fi
 
 if [ "x$CITELLUS_LIVE" = "x1" ];  then
-    NUM_NODES=$(pcs status | sed -n -r -e 's/^([0-9])[ \t]+node.*/\1/p')
+    NUM_NODES=$(pcs config |  awk '/Pacemaker Nodes/ {getline; print $0}' | wc -w)
     count_nodes
 elif [ "x$CITELLUS_LIVE" = "x0" ];  then
     if is_active "pacemaker"; then
@@ -49,8 +49,8 @@ elif [ "x$CITELLUS_LIVE" = "x0" ];  then
                 PCS_DIRECTORY="${CITELLUS_ROOT}/sos_commands/${CLUSTER_DIRECTORY}"
             fi
         done
-        is_required_file "${PCS_DIRECTORY}/pcs_status"
-        NUM_NODES=$(sed -n -r -e 's/^([0-9])[ \t]+node.*/\1/p' "${PCS_DIRECTORY}/pcs_status")
+        is_required_file "${PCS_DIRECTORY}/pcs_config"
+        NUM_NODES=$(awk '/Pacemaker Nodes/ {getline; print $0}' "${PCS_DIRECTORY}/pcs_config" | wc -w)
         count_nodes
     fi
 fi
