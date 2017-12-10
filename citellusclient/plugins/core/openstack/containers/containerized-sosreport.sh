bf87724030476dc5763a94c4d8a33960538c8d96
Robin Cernin <cerninr@gmail.com>
2017-12-10 22:39:00 +1000

    [citellus][common-function] Add function to check for containerized environment
    Closes #292
    
    Change-Id: I36bff4fe4c552211100933758f994c94766351e6

--- citellusclient/plugins/core/openstack/containers/containerized-sosreport.sh
+++ citellusclient/plugins/core/openstack/containers/containerized-sosreport.sh
@@ -29,36 +29,29 @@ exitoudated(){
 # Find release
 RELEASE=$(discover_osp_version)
 
-# description: Checks if OSP12 deployment is using containers
+# description: Checks if OSP12 deployment is using containers and valid sosreport version
 
 # Containerized deployment is only supported option starting in OSP 12
-if [ ${RELEASE} -ge 12 ]; then
-    if [ -d "${CITELLUS_ROOT}/var/log/containers" ] && [ -d "${CITELLUS_ROOT}/var/lib/config-data" ]; then
+is_required_containerized
 
-        # Sosreport with container support is 3.4-9 or later
-        is_required_rpm sos
+# Sosreport with container support is 3.4-9 or later
+is_required_rpm sos
 
-        VERSION=$(is_rpm sos)
+VERSION=$(is_rpm sos)
 
-        MAJOR=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-([0-9]+).[0-9]+-[0-9]+.*$/\1/p')
-        MID=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-[0-9]+.([0-9]+)-[0-9]+.*$/\1/p')
-        MINOR=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-[0-9]+.[0-9]+-([0-9]+).*$/\1/p')
+MAJOR=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-([0-9]+).[0-9]+-[0-9]+.*$/\1/p')
+MID=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-[0-9]+.([0-9]+)-[0-9]+.*$/\1/p')
+MINOR=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-[0-9]+.[0-9]+-([0-9]+).*$/\1/p')
 
-        if [[ "${MAJOR}" -ge "3" ]]; then
-            if [[ "${MID}" -ge "4" ]]; then
-                if [[ "${MINOR}" -lt "9" ]]; then
-                    exitoudated
-                fi
-            else
-                exitoudated
-            fi
-        else
+if [[ "${MAJOR}" -ge "3" ]]; then
+    if [[ "${MID}" -ge "4" ]]; then
+        if [[ "${MINOR}" -lt "9" ]]; then
             exitoudated
         fi
-        exit $RC_OKAY
-
+    else
+        exitoudated
     fi
 else
-    echo "works only on OSP 12 and later" >&2
-    exit $RC_SKIPPED
+    exitoudated
 fi
+exit $RC_OKAY
