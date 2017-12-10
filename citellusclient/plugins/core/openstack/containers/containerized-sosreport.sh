6e1e8413157eb0b0b5bb90a33bba2bb658360ecf
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-12-10 13:15:27 +0100

    Report old sosreport for containerized deployments
    Closes #287
    
    Change-Id: Iccea87dc7b86ef6ff4d123683253043b0bf9fb9f

---
+++ citellusclient/plugins/core/openstack/containers/containerized-sosreport.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Copyright (C) 2017  Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+# Based on the code of Jean-Francois Saucier (jsaucier@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Load common functions
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
+
+exitoudated(){
+g    echo "outdated sosreport package ${VERSION}: Containerized deployments require updated release" >&2
+    exit $RC_FAILED
+}
+
+
+# Find release
+RELEASE=$(discover_osp_version)
+
+# description: Checks if OSP12 deployment is using containers
+
+# Containerized deployment is only supported option starting in OSP 12
+if [ ${RELEASE} -ge 12 ]; then
+    if [ -d "${CITELLUS_ROOT}/var/log/containers" ] && [ -d "${CITELLUS_ROOT}/var/lib/config-data" ]; then
+
+        # Sosreport with container support is 3.4-9 or later
+        is_required_rpm sos
+
+        VERSION=$(is_rpm sos)
+
+        MAJOR=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-([0-9]+).[0-9]+-[0-9]+.*$/\1/p')
+        MID=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-[0-9]+.([0-9]+)-[0-9]+.*$/\1/p')
+        MINOR=$(echo ${VERSION} | sed -n -r -e 's/^sos.*-[0-9]+.[0-9]+-([0-9]+).*$/\1/p')
+
+        if [[ "${MAJOR}" -ge "3" ]]; then
+            if [[ "${MID}" -ge "4" ]]; then
+                if [[ "${MINOR}" -lt "9" ]]; then
+                    exitoudated
+                fi
+            else
+                exitoudated
+            fi
+        else
+            exitoudated
+        fi
+        exit $RC_OKAY
+
+    fi
+else
+    echo "works only on OSP 12 and later" >&2
+    exit $RC_SKIPPED
+fi
