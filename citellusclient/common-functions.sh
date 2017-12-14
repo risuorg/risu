284bfbddb2d2d4bb487aa6681ff2674bea0d3e3e
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-12-14 10:02:21 +0100

    Add bash_ini_parser
    Closes #143
    
    Change-Id: I3abedeae7440f24ad5fa88c5f4766996286f5dd4

--- citellusclient/common-functions.sh
+++ citellusclient/common-functions.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
-# Description: This script contains common functions to be used by citellus plugins
-#
-# Copyright (C) 2017  Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
-#
+#!/bin/bash
+
+# Description: This script contains common functions loader
+
+# Copyright (C) 2017   Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -16,147 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Helper script to define location of various files.
-
-if [ "x$CITELLUS_LIVE" = "x0" ];  then
-
-    # List of systemd/systemctl_list-units files
-    systemctl_list_units=( "${CITELLUS_ROOT}/sos_commands/systemd/systemctl_list-units" "${CITELLUS_ROOT}/sos_commands/systemd/systemctl_list-units_--all" )
-
-    # find available one and use it, the ones at back with highest priority
-    for file in ${systemctl_list_units[@]}; do
-        [[ -f "${file}" ]] && systemctl_list_units_file="${file}"
-    done
-
-    # List of logs/journalctl files
-    journal=( "${CITELLUS_ROOT}/sos_commands/logs/journalctl_--no-pager_--boot" "${CITELLUS_ROOT}/sos_commands/logs/journalctl_--all_--this-boot_--no-pager" )
-
-    # find available one and use it, the ones at back with highest priority
-    for file in "${journal[@]}"; do
-        [[ -f "${file}" ]] && journalctl_file="${file}"
-    done
-fi
-
-is_required_file() {
-    for file in "$@"; do
-        if [[ ! -f $file ]];  then
-            # to remove the ${CITELLUS_ROOT} from the stderr.
-            file=${file#$CITELLUS_ROOT}
-            echo "required file $file not found." >&2
-            exit $RC_SKIPPED
-        fi
-    done
-}
-
-is_active() {
-    if [ "x$CITELLUS_LIVE" = "x1" ]; then
-        systemctl is-active "$1" > /dev/null 2>&1
-    elif [ "x$CITELLUS_LIVE" = "x0" ]; then
-        if [[ -f "${systemctl_list_units_file}" ]]; then
-            grep -q "$1.* active" "${systemctl_list_units_file}"
-        else
-            echo "required systemd files not found." >&2
-            exit $RC_SKIPPED
-        fi
-    fi
-}
-
-is_enabled() {
-    if [ "x$CITELLUS_LIVE" = "x1" ]; then
-        systemctl is-enabled "$1" > /dev/null 2>&1
-    elif [ "x$CITELLUS_LIVE" = "x0" ]; then
-        if [[ -f "${CITELLUS_ROOT}/sos_commands/systemd/systemctl_list-unit-files" ]]; then
-            grep -q "$1.* enabled" "${CITELLUS_ROOT}/sos_commands/systemd/systemctl_list-unit-files"
-        else
-            echo "required systemd files not found." >&2
-            exit $RC_SKIPPED
-        fi
-    fi
-}
-
-is_rpm(){
-    if [ "x$CITELLUS_LIVE" = "x1" ]; then
-        rpm -qa *$1*|egrep ^"$1"-[0-9]
-    elif [ "x$CITELLUS_LIVE" = "x0" ]; then
-        is_required_file "${CITELLUS_ROOT}/installed-rpms"
-        awk '{print $1}' "${CITELLUS_ROOT}/installed-rpms"|egrep ^"$1"-[0-9]
-    fi
-}
-
-is_required_rpm(){
-    if ! is_rpm $1 ; then
-        echo "required package $1 not found." >&2
-        exit $RC_SKIPPED
-    fi
-}
-
-discover_osp_version(){
-    RPM=$(is_rpm openstack-nova-common)
-    case ${RPM} in
-        openstack-nova-common-2014.*) echo 6 ;;
-        openstack-nova-common-2015.*) echo 7 ;;
-        openstack-nova-common-12.*) echo 8 ;;
-        openstack-nova-common-13.*) echo 9 ;;
-        openstack-nova-common-14.*) echo 10 ;;
-        openstack-nova-common-15.*) echo 11 ;;
-        openstack-nova-common-16.*) echo 12 ;;
-        *) echo 0 ;;
-    esac
-}
-
-name_osp_version(){
-    VERSION=$(discover_osp_version)
-    case ${VERSION} in
-        6) echo "juno" ;;
-        7) echo "kilo" ;;
-        8) echo "liberty" ;;
-        9) echo "mitaka" ;;
-        10) echo "newton" ;;
-        11) echo "ocata" ;;
-        12) echo "pike" ;;
-        *) echo "not recognized" ;;
-    esac
-}
-
-is_process(){
-    if [ "x$CITELLUS_LIVE" = "x1" ];  then
-        ps -elf | grep "$1" | grep -q -v grep
-    elif [ "x$CITELLUS_LIVE" = "x0" ];  then
-        grep -q "$1" "${CITELLUS_ROOT}/ps";
-    fi
-}
-
-is_lineinfile(){
-    # $1: regexp
-    # $*: files
-    [ -f "$2" ] && egrep -iq "$1" "${@:2}"
-}
-
-is_containerized(){
-    RELEASE=$(discover_osp_version)
-    [[ -d "${CITELLUS_ROOT}/var/log/containers" ]] && [[ -d "${CITELLUS_ROOT}/var/lib/config-data" ]]
-}
-
-is_required_containerized(){
-    if ! is_containerized; then
-        echo "the OSP${RELEASE} deployment seems to not be containerized" >&2
-        exit $RC_SKIPPED
-    fi
-}
-
-docker_runit(){
-    # Run command in docker container
-    # $1: container name
-    # $2: command
-    if [ "x$CITELLUS_LIVE" = "x1" ]; then
-        if [[ -x "$(which docker)" ]]; then
-            docker exec $(docker ps | grep "${1}" | cut -d" " -f1) sh -c "${@:2}"
-        else
-            echo "docker: command not found or executable" >&2
-            exit $RC_SKIPPED
-        fi
-    elif [ "x$CITELLUS_LIVE" = "x0" ]; then
-        echo "docker: works only on live system" >&2
-        exit $RC_SKIPPED
-    fi
-}
+# Load all common functions defined in common.d
+for file in $(find common.d -name .sh);do
+    . $file
+done
