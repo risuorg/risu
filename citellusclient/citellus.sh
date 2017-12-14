3a5acd1391cca91821bcb2897d1285a2cdc90891
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-12-14 10:02:21 +0100

    Add bash_ini_parser
    Closes #143
    
    Change-Id: I3abedeae7440f24ad5fa88c5f4766996286f5dd4

--- citellusclient/citellus.sh
+++ citellusclient/citellus.sh
@@ -18,24 +18,6 @@
 
 # this can run against live and also any sort of snapshot of the filesystem
 
-export LANG=en_US
-
-TEST_OKAY=$(tput setaf 2; echo "okay"; tput sgr0)
-TEST_SKIPPED=$(tput setaf 3; echo "skipped"; tput sgr0)
-TEST_FAILED=$(tput setaf 1; echo "failed"; tput sgr0)
-TEST_WTF=$(tput setaf 1; echo "unexpected result"; tput sgr0)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Add the extra vars we added on citelus.py to keep some
-# level of compatibility for this script to keep working as fallback
-
-export CITELLUS_BASE=${DIR}
-export RC_OKAY=10
-export RC_FAILED=20
-export RC_SKIPPED=30
-export TEXTDOMAIN='citellus'
-export TEXTDOMAINDIR=${CITELLUS_BASE}/locale
-
 scriptname() {
     echo "${0##*/}: $1" >&2
 }
