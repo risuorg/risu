d7ed42da575563a7a0ee7d5b1d78bbdc30ad7da0
Robin Cernin <cerninr@gmail.com>
2017-12-21 17:11:57 +1000

    [citellus][metadata][virtualization] Adding metadata to virtualization scripts
    Change-Id: Ib84e9361780218afb95a63fe32017198eff00fa8

--- citellusclient/plugins/core/virtualization/libvirt-qemu-kernel-compatibility.sh
+++ citellusclient/plugins/core/virtualization/libvirt-qemu-kernel-compatibility.sh
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# long_name: Kernel, QEMU and Libvirt compatibility
 # description: Checks compatibility between libvirt, qemu and kernel packages
 
 # Load common functions
