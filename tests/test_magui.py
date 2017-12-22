0f81edffc3937e6a6acc96aba686ad0f207217bd
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2017-12-22 09:24:43 +0100

    Add extra metadata to Magui output based on recent citellus changes
    Closes #322
    
    Change-Id: I0773f73d06a21d0ba208e8acb732827d22ef84c0

--- tests/test_magui.py
+++ tests/test_magui.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-
 import os
 from unittest import TestCase
 
@@ -41,4 +40,4 @@ class MaguiTest(TestCase):
 
         # Call with no arguments should return empty
         res = magui.maguiformat(magui.domagui(sosreports=sosreports, citellusplugins=plugins))
-        assert res == {'/exit_failed.sh': {'host1': {'err': testplugins + u'/exit_failed.sh something on stderr\n', 'out': testplugins + u'/exit_failed.sh something on stdout\n', 'rc': citellus.RC_FAILED}, 'host2': {'err': testplugins + u'/exit_failed.sh something on stderr\n', 'out': testplugins + u'/exit_failed.sh something on stdout\n', 'rc': citellus.RC_FAILED}}, '/exit_unknown.sh': {'host1': {'err': testplugins + u'/exit_unknown.sh something on stderr\n', 'out': testplugins + u'/exit_unknown.sh something on stdout\n', 'rc': 99}, 'host2': {'err': testplugins + u'/exit_unknown.sh something on stderr\n', 'out': testplugins + u'/exit_unknown.sh something on stdout\n', 'rc': 99}}}
+        assert res == {'/exit_unknown.sh': {'sosreport': {'bugzilla': '', 'backend': 'core', 'long_name': '', 'sosreport': {'host1': {'err': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_unknown.sh something on stderr\n', 'rc': 99, 'out': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_unknown.sh something on stdout\n'}, 'host2': {'err': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_unknown.sh something on stderr\n', 'rc': 99, 'out': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_unknown.sh something on stdout\n'}}, 'description': ''}}, '/exit_failed.sh': {'sosreport': {'bugzilla': '', 'backend': 'core', 'long_name': '', 'sosreport': {'host1': {'err': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_failed.sh something on stderr\n', 'rc': 20, 'out': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_failed.sh something on stdout\n'}, 'host2': {'err': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_failed.sh something on stderr\n', 'rc': 20, 'out': u'/home/iranzo/DEVEL/citellus/citellusclient/plugins/test/exit_failed.sh something on stdout\n'}}, 'description': ''}}}
