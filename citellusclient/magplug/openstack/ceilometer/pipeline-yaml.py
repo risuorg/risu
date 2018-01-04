f8f40369d04689ac4fdb07dd468aa2cd8c1bd92a
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-04 12:58:19 +0100

    Do analisys on citellus data within magui and report wrong pipeline.yaml
    Closes #334
    Closes #329
    
    Change-Id: Ica6789de2385bdf8c55ddaed93dc9b13e478152e

---
+++ citellusclient/magplug/openstack/ceilometer/pipeline-yaml.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (C) 2018  Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+# Description: Plugin for checking ceilimeter pipeline-yaml data
+
+from __future__ import print_function
+
+try:
+    import citellusclient.shell as citellus
+except:
+    import shell as citellus
+
+# Load i18n settings from citellus
+_ = citellus._
+
+extension = "pipeline-yaml"
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for Plugin
+    """
+    triggers = []
+    return triggers
+
+
+def run(data, quiet=False):  # do not edit this line
+    """
+    Executes plugin
+    :param plugin: plugin dictionary
+    :return: returncode, out, err
+    """
+
+    # Plugin ID to act on:
+    plugid = "131c0e0d785fae9811f2754262f0da9e"
+
+    ourdata = False
+    for item in data:
+        if data[item]['id'] == plugid:
+            ourdata = data[item]
+
+    if ourdata:
+        # 'err' in this case should be always equal to the md5sum of the file so that we can report the problem
+        err = []
+        for sosreport in ourdata['sosreport']:
+            err.append(ourdata['sosreport'][sosreport]['err'])
+
+    message = []
+    if len(sorted(set(err))) != 1:
+        message = _("Pipeline.yaml contents differ across sosreports, please do check that the contents are the same and shared across the environment to ensure proper behavior.")
+
+    return message
+
+
+def help():  # do not edit this line
+    """
+    Returns help for plugin
+    :return: help text
+    """
+
+    commandtext = _("This plugin checks Ceilometer pipeline.yaml consistency across sosreports")
+    return commandtext
