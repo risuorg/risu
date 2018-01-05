dd4fc6f53592d9504fe4ea62d2ec14104ab87663
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-05 23:17:19 +0100

    Add function to check if rpm is over a specific version or exit failing
    Change-Id: I8995ec39d06c7cb11482c150a15bf9efce1e1ff1

---
+++ citellusclient/common.d/01-rpm.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# Description: This script contains common functions to be used by citellus plugins
+#
+# Copyright (C) 2018  Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+is_rpm(){
+    if [ "x$CITELLUS_LIVE" = "x1" ]; then
+        rpm -qa *$1*|egrep ^"$1"-[0-9]
+    elif [ "x$CITELLUS_LIVE" = "x0" ]; then
+        is_required_file "${CITELLUS_ROOT}/installed-rpms"
+        awk '{print $1}' "${CITELLUS_ROOT}/installed-rpms"|egrep ^"$1"-[0-9]
+    fi
+}
+
+is_required_rpm(){
+    if ! is_rpm $1 ; then
+        echo "required package $1 not found." >&2
+        exit $RC_SKIPPED
+    fi
+}
+
+is_rpm_over(){
+    # $1 RPM
+    # $2 MAJOR
+    # $3 MID
+    # $4 MINOR
+    is_required_rpm $1
+    VERSION=$(is_rpm sos)
+    MAJOR=$(echo ${VERSION} | sed -n -r -e "s/^$1.*-([0-9]+).[0-9]+-[0-9]+.*$/\1/p")
+    MID=$(echo ${VERSION} | sed -n -r -e "s/^$1.*-[0-9]+.([0-9]+)-[0-9]+.*$/\1/p")
+    MINOR=$(echo ${VERSION} | sed -n -r -e "s/^$1.*-[0-9]+.[0-9]+-([0-9]+).*$/\1/p")
+
+    if [[ "${MAJOR}" -ge "$2" ]]; then
+        if [[ "${MID}" -ge "$3" ]]; then
+            if [[ "${MINOR}" -lt "$4" ]]; then
+                echo "required package $1 version is lower than $MAJOR $MID $MINOR." >&2
+                exit $RC_FAILED
+            fi
+        else
+            echo "required package $1 version is lower than $MAJOR $MID $MINOR." >&2
+            exit $RC_FAILED
+        fi
+    else
+        echo "required package $1 version is lower than $MAJOR $MID $MINOR." >&2
+        exit $RC_FAILED
+    fi
+}
