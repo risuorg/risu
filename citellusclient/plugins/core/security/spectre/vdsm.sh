d4bb62b3f83cd91d3e400bd0f302678ab185c4ed
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-08 11:26:38 +0100

    Checks for Spectre/Meltdown issues
    Closes #345
    
    Change-Id: Ib0747da4ed7454a851e16111b65028a038187cea

---
+++ citellusclient/plugins/core/security/spectre/vdsm.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright (C) 2018  Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: Checks for fixed dracut package
+# description: Checks if package is affected of Spectre/Meltdown
+
+# Load common functions
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
+
+exitoudated(){
+    echo "Please do check https://access.redhat.com/security/vulnerabilities/speculativeexecution for guidance" >&2
+}
+RELEASE=$(discover_rhrelease)
+if [[ "${RELEASE}" -eq "7" ]]; then
+    exitoudated
+    is_rpm_over vdsm 4 19 43 3
+fi
+exit $RC_OKAY
