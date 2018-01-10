baf1f4e09ec0cb87b01c2359c96c6f5739597813
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-10 21:59:47 +0100

    Initial support for metadata extension.
    Target is to set variables that can later be used by magui for example to detect sosreports for the same host at different dates for comparison.
    
    Change-Id: I28d01b75547cc96cbf3b27727030cad1acea8a57

---
+++ citellusclient/extensions/metadata.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Extension for running and reporting metadata
+# Author: Pablo Iranzo Gomez (Pablo.Iranzo@gmail.com)
+
+from __future__ import print_function
+
+import os
+
+try:
+    import citellusclient.shell as citellus
+except:
+    import shell as citellus
+
+# Load i18n settings from citellus
+_ = citellus._
+
+extension = "metadata"
+pluginsdir = os.path.join(citellus.citellusdir, 'plugins', extension)
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for extension
+    """
+    triggers = ['metadata']
+    return triggers
+
+
+def listplugins(options):
+    """
+    List available plugins
+    :param options: argparse options provided
+    :return: plugin object generator
+    """
+    yield citellus.findplugins(folders=[pluginsdir], include=options.include, exclude=options.exclude, extension='metadata')
+
+
+def get_metadata(plugin):
+    """
+    Gets metadata for plugin
+    :param plugin: plugin object
+    :return: metadata dict for that plugin
+    """
+
+    metadata = {'description': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# description:')[14:].strip(),
+                'long_name': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# long_name:')[12:].strip()}
+    return metadata
+
+
+def run(plugin):  # do not edit this line
+    """
+    Executes plugin
+    :return: returncode, out, err
+    """
+    return citellus.execonshell(filename=plugin['plugin'])
+
+
+def help():  # do not edit this line
+    """
+    Returns help for plugin
+    :return: help text
+    """
+
+    commandtext = _("This extension proceses Citellus metadata plugins to fill details about system")
+    return commandtext
