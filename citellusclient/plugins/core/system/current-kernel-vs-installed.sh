32de865e92e68adcd82aff6d80fc86922e98a2e5
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-10 16:33:00 +0100

    Move to use RPMDB instead of yum.log
    Closes #361
    
    Change-Id: I7bc81183d5a879c16798ba01100f7f226cee81f5

--- citellusclient/plugins/core/system/current-kernel-vs-installed.sh
+++ citellusclient/plugins/core/system/current-kernel-vs-installed.sh
@@ -25,24 +25,27 @@
 
 if [ "x$CITELLUS_LIVE" = "x0" ]; then
     FILE="${CITELLUS_ROOT}/uname"
+    RPMFILE="${CITELLUS_ROOT}/installed-rpms"
 elif [ "x$CITELLUS_LIVE" = "x1" ];then
     FILE=$(mktemp)
-    trap "rm $FILE" EXIT
+    RPMFILE=$(mktemp)
+    trap "rm $FILE $RPMFILE" EXIT
     uname -a > $FILE
+    rpm -qa kernel* > $RPMFILE
 fi
 
 is_required_file "$FILE"
 running_kernel=$(cut -d" " -f3 "$FILE" | sed -r 's/(^([0-9]+\.){2}[0-9]+-[0-9]+).*$/\1/')
 
 is_required_file "${CITELLUS_ROOT}/var/log/yum.log"
-nkernel=$(grep "\skernel-[0-9]" "${CITELLUS_ROOT}/var/log/yum.log")
+nkernel=$(grep "\skernel-[0-9]" "${RPMFILE}")
 if [[ -z "$nkernel" ]]; then
     echo $"no kernel install or update found in yum.log"
     exit $RC_OKAY
 fi
 
-installed_kernel=$(grep "\skernel-[0-9]" "${CITELLUS_ROOT}/var/log/yum.log" | tail -1 \
-    | awk '{print $NF}' |  sed -r 's/[a-z]*-(([0-9]+\.){2}[0-9]+-[0-9]+).*$/\1/')
+installed_kernel=$(grep "\skernel-[0-9]" "${RPMFILE}" | sort -V| tail -1 \
+    | awk '{print $NF}'|  sed -r 's/[a-z]*-(([0-9]+\.){2}[0-9]+-[0-9]+).*$/\1/')
 
 
 if [[ "$running_kernel" == "$installed_kernel" ]]; then
