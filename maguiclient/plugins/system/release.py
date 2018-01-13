c1598874017fe639dfdf925274d65b357eba92d0
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-13 13:59:53 +0100

    Adds plugin to check metadata of different RHEL releases
    Closes #371
    
    Change-Id: I7733ac3b5b4d0ad8e4772c7b61872d131a382a01

---
+++ maguiclient/plugins/system/release.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (C) 2018  Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+# Description: Plugin for checking RH Release across hosts
+
+from __future__ import print_function
+
+try:
+    import citellusclient.shell as citellus
+except:
+    import shell as citellus
+
+# Load i18n settings from citellus
+_ = citellus._
+
+extension = "release"
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for Plugin
+    """
+    triggers = ['1586bab95f7d891153bcbb6da148e361']
+    return triggers
+
+
+def run(data, quiet=False):  # do not edit this line
+    """
+    Executes plugin
+    :param quiet: reduce amount of data returned
+    :param data: data to process
+    :return: returncode, out, err
+    """
+
+    returncode = citellus.RC_OKAY
+
+    message = ''
+    for ourdata in data:
+        # 'err' in this case should be always equal to the md5sum of the file so that we can report the problem
+        err = []
+        for sosreport in ourdata['sosreport']:
+            err.append(ourdata['sosreport'][sosreport]['err'])
+
+        if len(sorted(set(err))) != 1:
+            message = _("Hosts contains different releases of operating system and can cause issues.")
+            returncode = citellus.RC_FAILED
+
+    out = ''
+    err = message
+    return returncode, out, err
+
+
+def help():  # do not edit this line
+    """
+    Returns help for plugin
+    :return: help text
+    """
+
+    commandtext = _("This plugin checks uniform releasing of OS across sosreports")
+    return commandtext
