823595bdb8d704c83820960a1876cb9783552d49
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-24 22:47:16 +0100

    Check that no 'if' is using single brackets or parenthesis
    Closes #410
    
    Change-Id: I96c9eed0455c8245b628755e2837d58a6d1600bd

--- tests/test_citellus.py
+++ tests/test_citellus.py
@@ -129,3 +129,25 @@ class CitellusTest(TestCase):
     def test_readconfig(self):
         parsed = citellus.read_config()
         assert parsed == {}
+
+    def test_plugins_have_dual_brackets_for_if(self):
+        pluginpath = [os.path.join(citellus.citellusdir, 'plugins', 'core')]
+        pluginscit = []
+        for plugin in citellus.findplugins(folders=pluginpath):
+            filename = plugin['plugin']
+            regexp = 'if \[ '
+            if citellus.regexpfile(filename=filename, regexp=regexp):
+                pluginscit.append(filename)
+
+        assert len(pluginscit) == 0
+
+    def test_plugins_have_dual_parenthesis_for_if(self):
+        pluginpath = [os.path.join(citellus.citellusdir, 'plugins', 'core')]
+        pluginscit = []
+        for plugin in citellus.findplugins(folders=pluginpath):
+            filename = plugin['plugin']
+            regexp = 'if \( '
+            if citellus.regexpfile(filename=filename, regexp=regexp):
+                pluginscit.append(filename)
+
+        assert len(pluginscit) == 0
