2fc3d1166b1bb16b3aba36a0cf8ef9159341121f
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-25 00:54:07 +0100

    Introduce Faraday extension for replacing file affinity/anti-affinity checks
    Change-Id: I25781fe9db760e305b45f6c233923e54d95fb0b4
    Closes: #406
    Closes: #413

---
+++ citellusclient/extensions/faraday.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Extension for processing file affinities/antiaffinities to be reported in a
+#              similar way to metadata and later processed by corresponding plugin in Magui
+#
+# Author: Pablo Iranzo Gomez (Pablo.Iranzo@gmail.com)
+
+from __future__ import print_function
+
+import os
+import hashlib
+
+try:
+    import citellusclient.shell as citellus
+except:
+    import shell as citellus
+
+# Load i18n settings from citellus
+_ = citellus._
+
+extension = "faraday"
+pluginsdir = os.path.join(citellus.citellusdir, 'plugins', extension)
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for extension
+    """
+    triggers = ['faraday']
+    return triggers
+
+
+def listplugins(options):
+    """
+    List available plugins
+    :param options: argparse options provided
+    :return: plugin object generator
+    """
+
+    yield citellus.findplugins(folders=[pluginsdir], executables=False, fileextension=".txt", extension=extension, prio=options.prio)
+
+
+def get_metadata(plugin):
+    """
+    Gets metadata for plugin
+    :param plugin: plugin object
+    :return: metadata dict for that plugin
+    """
+
+    metadata = {'description': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# description:')[14:].strip(),
+                'long_name': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# long_name:')[12:].strip(),
+                'bugzilla': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# bugzilla:')[11:].strip(),
+                'priority': int(citellus.regexpfile(filename=plugin['plugin'], regexp='\A# priority:')[11:].strip() or 0),
+                'path': citellus.regexpfile(filename=plugin['plugin'], regexp='\A# path:')[8:].strip() or 0}
+    return metadata
+
+
+def run(plugin):  # do not edit this line
+    """
+    Executes plugin
+    :return: returncode, out, err
+    """
+    filename = plugin['path']
+    if '${CITELLUS_ROOT}' in filename:
+        filename = os.path.join(os.environ['CITELLUS_ROOT'], filename.replace('${CITELLUS_ROOT}', ''))
+    if os.access(filename, os.R_OK):
+        # We can read the file, so let's calculate md5sum
+        out = ''
+        err = hashlib.md5(open(filename, 'rb').read()).hexdigest()
+        returncode = citellus.RC_OKAY
+    else:
+        returncode = citellus.RC_SKIPPED
+        out = ''
+        err = 'File %s is not accessible in read mode' % filename
+
+    return returncode, out, err
+
+
+def help():  # do not edit this line
+    """
+    Returns help for plugin
+    :return: help text
+    """
+
+    commandtext = _("This extension creates fake plugins based on affinity/antiaffinity file list for later processing")
+    return commandtext
