77396aa9eb3ff7bc8a7d1e9146884dadba89442e
David Vallee Delisle <dvd@redhat.com>
2018-01-24 21:51:04 -0500

    Extracting system uuid in metadata
    Change-Id: I1bd2e4f9f64204e2eefe077fab55b6ca9799608a

---
+++ citellusclient/plugins/metadata/system/system-uuid.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright (C) 2018 David Vallee Delisle (dvd@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: stores system UUID for correlation in webapp
+# description: Sets system UUID
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if [[ $CITELLUS_LIVE -eq 0 ]]; then
+    UUID=$(cat ${CITELLUS_ROOT}/dmidecode | grep -oP "UUID: \K(.*)")
+elif [[ $CITELLUS_LIVE -eq 1 ]];then
+    UUID=$(sudo dmidecode | grep -oP "UUID: \K(.*)")
+fi
+
+echo $UUID >&2
+exit $RC_OKAY
