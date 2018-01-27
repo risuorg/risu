950de9857e4d748cd6534be36d6fd6f226c9dab0
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-01-27 16:26:27 +0100

    Fix some changes in faraday
    Change-Id: I5557ddeb437cdff268b70b11f33e4bb9fdc28a70

--- citellusclient/extensions/faraday.py
+++ citellusclient/extensions/faraday.py
@@ -71,19 +71,18 @@ def run(plugin):  # do not edit this line
     """
     filename = plugin['path']
 
-    if citellus.CITELLUS_LIVE == 0 and citellus.regexpfile(filename=plugin['plugin'], regexp="CITELLUS_ROOT"):
-        # We're running in snapshoot and farday file has CITELLUS_ROOT
+    skipped = 0
+    if os.environ['CITELLUS_LIVE'] == 0 and citellus.regexpfile(filename=filename, regexp="CITELLUS_ROOT"):
+        # We're running in snapshoot and faraday file has CITELLUS_ROOT
         skipped = 0
-    elif citellus.CITELLUS_LIVE == 1:
-        if citellus.regexpfile(filename=plugin['plugin'], regexp="CITELLUS_HYBRID") or not citellus.regexpfile(filename=plugin['plugin'], regexp="CITELLUS_ROOT"):
-            # We're running in Live mode and either plugin supports HYBRID or has no CITELLUS_ROOT
-            skipped = 0
-        else:
-            # We do not satisfy conditions, exit early
-            skipped = 1
     else:
-            # We do not satisfy conditions, exit early
-            skipped = 1
+        if os.environ['CITELLUS_LIVE'] == 1:
+            if citellus.regexpfile(filename=plugin['plugin'], regexp="CITELLUS_HYBRID") or not citellus.regexpfile(filename=filename, regexp="CITELLUS_ROOT"):
+                # We're running in Live mode and either plugin supports HYBRID or has no CITELLUS_ROOT
+                skipped = 0
+            else:
+                # We do not satisfy conditions, exit early
+                skipped = 1
 
     if skipped == 1:
         return citellus.RC_SKIPPED, '', _('Plugin does not satisfy conditions for running')
