8cb4cc0baa84ef9017999b849356864e64569a0d
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-02-08 22:39:03 +0100

    Adds extra check for running only on controllers
    Closes #449
    
    Change-Id: I28e9cd5d897c140feab5e8ba66243c955dfebe67

--- citellusclient/plugins/core/openstack/keystone/cleanup_runs.sh
+++ citellusclient/plugins/core/openstack/keystone/cleanup_runs.sh
@@ -24,18 +24,21 @@
 # Load common functions
 [[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
 
-if is_process nova-compute; then
-    echo "works only on controller node" >&2
+is_required_file "${CITELLUS_ROOT}/var/log/keystone/keystone.log"
+
+# Check if we've the keystone token manage cleanup job so we asume it's controller
+is_required_file "${CITELLUS_ROOT}/var/spool/cron/keystone"
+if ! is_lineinfile keystone-manage "${CITELLUS_ROOT}/var/spool/cron/keystone"; then
+    echo "Only runs on OSP controller" >&2
     exit $RC_SKIPPED
 fi
 
-is_required_file "${CITELLUS_ROOT}/var/log/keystone/keystone.log"
-
 RUNS=$(grep 'Total expired tokens removed' "${CITELLUS_ROOT}/var/log/keystone/keystone.log" | wc -l)
 
-[[ "x${RUNS}" = "x" ]] && exit $RC_FAILED
+[[ "x${RUNS}" = "x" ]] && echo "Non recorded cleanup runs" >&2 && exit $RC_FAILED
 
 if [[ "${RUNS}" -eq 0 ]]; then
+    echo "Non recorded cleanup runs" >&2
     exit $RC_FAILED
 elif [[ "${RUNS}" -ge 1 ]]; then
     echo "${RUNS}" >&2
