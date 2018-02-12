799c635999a1f118eb80dbdc4e421c55ce0a6d23
Mikel Olasagasti Uranga <mikel@olasagasti.info>
2018-02-12 12:47:41 +0100

    [plugins][core][system] Check PERC RAID / Megaraid resets
    Change-Id: I970d5c580807e645b904df1c40bcb40cfe3d12de

---
+++ citellusclient/plugins/core/system/megaraid.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Mikel Olasagasti Uranga (mikel@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: Detects PERC RAID / Megaraid resets
+# description: Detects if PERC RAID Controller or Linux Megaraid Driver resets resulting in intermittent loss of access to all drivers on system
+# priority: 500
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+ERRORMSG=$"PERC RAID / Megaraid reset detected"
+ERRORMATCH="megaraid_sas: resetting fusion adapter"
+
+if [[ "x$CITELLUS_LIVE" = "x0" ]];  then
+    is_required_file "${CITELLUS_ROOT}/var/log/messages"
+    if is_lineinfile $ERRORMATCH "${CITELLUS_ROOT}/var/log/messages"; then
+        echo $ERRORMSG >&2
+        exit $RC_FAILED
+    fi
+elif [[ "x$CITELLUS_LIVE" = "x1" ]]; then
+    errcount=$(zgrep $ERRORMATCH /var/log/messages* |wc -l)
+    if [[ $errcount -gt 0 ]] ; then
+        echo $ERRORMSG >&2
+        exit $RC_FAILED
+    fi
+fi
+
+# exit as OK if haven't failed earlier
+exit $RC_OKAY
