af2f6cd7e40332406c13aafbbbe5c13aef5bcf1e
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-03-01 23:37:07 +0100

    Use travis build as version
    Change-Id: I31be5ad570039b793dbd513683e82dc2bf67f085

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # encoding: utf-8
 
 import setuptools
+import os
+import re
 
 # In python < 2.7.4, a lazy loading of package `pbr` will break
 # setuptools if some other modules registered functions in `atexit`.
@@ -11,5 +13,23 @@ try:
     import multiprocessing  # noqa
 except ImportError:
     pass
+
+filename = 'setup.cfg'
+regexp = '\Aversion.*([0-9]+)'
+
+with open(filename, 'r') as f:
+    for line in f:
+        if re.match(regexp, line):
+            # Return earlier if match found
+            break
+
+version = line.split("=")[1].strip()
+try:
+    travis = os.environ['TRAVIS_JOB_ID']
+except:
+    travis = None
+
+if travis:
+    os.environ['PBR_VERSION'] = "%s.%s" % (version, travis)
 
 setuptools.setup(setup_requires=['pbr>=2.0.0'], pbr=True)
