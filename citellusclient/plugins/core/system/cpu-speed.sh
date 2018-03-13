38fe2e06246684d33e9335ec0fc1ae79c7530e66
Robin Cernin <rcernin@redhat.com>
2018-03-13 10:14:33 +1000

    [citellus] Max CPU speed and C-States
    Closes: #500
    
    Change-Id: Ic51d78b774bed787146f3b95f94b60ad28740910

---
+++ citellusclient/plugins/core/system/cpu-speed.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright (C) 2018   Robin Černín (rcernin@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: Max CPU speed and C-states
+# description: Checks for CPU speed and C-states
+# priority: 200
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+# check baremetal node
+
+is_required_file "${CITELLUS_ROOT}/sys/module/intel_idle/parameters/max_cstate" \
+                 "${CITELLUS_ROOT}/proc/cpuinfo"
+
+cstate=$(cat "${CITELLUS_ROOT}/sys/module/intel_idle/parameters/max_cstate")
+
+if [[ "${cstate}" -ne "0" ]]; then
+    echo $"CPU is not running at full speed" >&2
+    grep "cpu MHz" "${CITELLUS_ROOT}/proc/cpuinfo" >&2
+    exit ${RC_FAILED}
+else
+    exit ${RC_OKAY}
+fi
