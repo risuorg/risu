a1d68879f2fe6803c1b3614607a4ab434f555847
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-03-18 21:48:48 +0100

    Report pacemaker ip for node
    Change-Id: Ia58a15b73832995f29d74a4e71fe44e2376f57a5

---
+++ citellusclient/plugins/metadata/pacemaker/node-pacemaker-ip.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright (C) 2018   Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: prepares hostname metadata
+# description: Sets hostname metadata
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if [[ ${CITELLUS_LIVE} -eq 0 ]]; then
+    FILE="${CITELLUS_ROOT}/hostname"
+elif [[ ${CITELLUS_LIVE} -eq 1 ]];then
+    FILE=$(mktemp)
+    trap "rm ${FILE}" EXIT
+    hostname  > ${FILE}
+fi
+
+
+# Fill metadata 'hostname' to value
+echo "node-pacemaker-ip"
+grep "$(cat ${FILE})" "${CITELLUS_ROOT}/etc/hosts"|awk '{print $1}' >&2
+exit ${RC_OKAY}
