595c8b8ebc584b7ffb97d392dbf5a49d3bdb941d
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-03-20 11:55:52 +0100

    BZ 1554265
    Change-Id: Ia6aa81e1ffefec6e150f3d0e5b69c401e061f508

---
+++ citellusclient/plugins/core/bugzilla/openstack/nova/1554265.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# this can run against live and also any sort of snapshot of the filesystem
+
+# long_name: Verify preallocate images setting in nova.conf
+# description: Verify preallocate images setting
+# bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1554265
+# priority: 500
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+is_required_file "${CITELLUS_ROOT}/etc/nova/nova.conf"
+
+allocation=$(iniparser "${CITELLUS_ROOT}/etc/nova/nova.conf" DEFAULT preallocate_images)
+
+if [[ "${allocation}" == 'space' ]]; then
+    echo $"https://bugzilla.redhat.com/show_bug.cgi?id=1554265" >&2
+    exit ${RC_FAILED}
+else
+    exit ${RC_OKAY}
+fi
