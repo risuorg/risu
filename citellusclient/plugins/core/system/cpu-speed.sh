dd27e44513858865fe1c211d1ccd53164fcfbd0e
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-03-27 08:45:11 +0200

    Show processor ID in output
    Change-Id: I476046660f0c56af510916495ba8d1213bd2de04

--- citellusclient/plugins/core/system/cpu-speed.sh
+++ citellusclient/plugins/core/system/cpu-speed.sh
@@ -31,7 +31,7 @@ cstate=$(cat "${CITELLUS_ROOT}/sys/module/intel_idle/parameters/max_cstate")
 
 if [[ "${cstate}" -ne "0" ]]; then
     echo $"CPU is not running at full speed" >&2
-    grep "cpu MHz" "${CITELLUS_ROOT}/proc/cpuinfo" >&2
+    egrep "processor|cpu MHz" "${CITELLUS_ROOT}/proc/cpuinfo" | tr '\n' ' '|sed 's/processor/\nprocessor/g' >&2
     exit ${RC_FAILED}
 else
     exit ${RC_OKAY}
