ac4e84ca5034fa5639ca9257fb83352928e5b4b4
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-04-13 13:43:16 +0200

    [plugins][openstack][openvswitch] Checks ports in no namespace
    Closes #558
    
    Change-Id: Id84237ff0995fea943ec1007313754fa0b94e721

---
+++ citellusclient/plugins/core/openstack/openvswitch/ports-in-no-namespace.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Copyright (C) 2018   Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+# Based on code snippet by Miguel Ángel Ajo Pelayo (majopela@redhat.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# we can run this against fs snapshot or live system
+
+# long_name: Check for ovs ports in no namespace
+# description: Checks for ovs ports in no namespace
+# priority: 800
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if [[ ${CITELLUS_LIVE} -eq 0 ]]; then
+    FILE="${CITELLUS_ROOT}/sos_commands/openvswitch/ovsdb-client_-f_list_dump"
+    is_required_directory "${CITELLUS_ROOT}/sos_commands/networking/"
+    NICS="$(ls ${CITELLUS_ROOT}/sos_commands/networking/)"
+elif [[ ${CITELLUS_LIVE} -eq 1 ]];then
+    FILE=$(mktemp)
+    trap "rm ${FILE}" EXIT
+    ovsdb-client -f list dump > ${FILE}
+    NICS=$(ls /proc/sys/net/ipv*/conf |grep -v "sys/net"|sort|uniq)
+fi
+
+is_required_file "${FILE}"
+
+PREPORTS="$(cat ${FILE}|egrep '(ha-|qr-|gq-)')"
+PREPORTS="${PREPORTS} $(cat ${FILE}|grep name|grep tap)"
+
+PORTS=`echo ${PREPORTS}|cut -d\" -f2`
+
+echo "ports not in any router or namespace" >&2
+flag=0
+
+for port in ${PORTS}; do
+         echo ${NICS} | grep $port &> /dev/null
+        if [[ "$?" != "0" ]]; then
+            echo $port >&2
+            flag=1
+        fi
+done
+
+if [[ ${flag} -eq '1' ]]; then
+    exit ${RC_FAILED}
+fi
+
+exit ${RC_OKAY}
