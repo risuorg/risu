3744de217f12cf7943e684bc07c544d5009bdebe
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-05-07 14:53:19 +0200

    Move functions for expanding ranges to common function set
    Change-Id: Ifdca217cfb440e0bdd8cc9a4314300ca2698de78

--- citellusclient/common.d/00-core.sh
+++ citellusclient/common.d/00-core.sh
@@ -179,3 +179,35 @@ strip_and_trim() {
     local file="$1"
     egrep -v "^\s*($|#.*)" $file | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//'
 }
+
+expand_ranges(){
+    (
+    for CPU in $(echo $*|tr "," "\n");do
+        if [[ "$CPU" == *"-"* ]];then
+            echo ${CPU}|awk -F "-" '{print $1" "$2}'|xargs seq
+        else
+            echo "$CPU"
+        fi
+    done
+    )|xargs echo
+}
+
+expand_and_remove_excludes(){
+    RANGE=$(expand_ranges $*)
+    CPUs=$(echo ${RANGE}|tr " " "\n"|egrep -v "\^.*")
+    EXCLUDES=$(echo ${RANGE}|tr " " "\n"|egrep "\^.*")
+    (
+    for CPU in ${CPUs}; do
+        exclude=0
+        for EXCL in ${EXCLUDES};do
+            if [[ "^$CPU" == "$EXCL" ]]; then
+                exclude=1
+            fi
+        done
+        if [[ "$exclude" == "0" ]];then
+            echo ${CPU}
+        fi
+
+    done
+    )|xargs echo
+}
