148f6e823738a09ae8900b998d89b3031a2f4660
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-05-09 16:19:33 +0200

    Initial add of kbases from sumsos based on the regexps from John Devereux
    Tests are based on templates and should need to be adjusted in priority, etc
    
    Closes #583
    
    Change-Id: I4b572e3ab6a498cce3010215b42eb6c73b327894

---
+++ citellusclient/plugins/core/sumsos/kbases/sumsos-kbase-232283-32.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+# Code based on the regexps from sumsos from John Devereux (john_devereux@yahoo.com)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# we can run this against fs snapshot or live system
+
+# long_name: Finds matches of lvm.*: Unmounting invalid snapshot .* from .*
+# description: Finds matches of error lvm.*: Unmounting invalid snapshot .* from .*
+
+REGEXP="lvm.*: Unmounting invalid snapshot .* from .*"
+KCS=232283
+
+
+# priority: 500
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if [[ "x$CITELLUS_LIVE" = "x0" ]]; then
+    journal="$journalctl_file"
+else
+    journal="$(mktemp)"
+    trap "/bin/rm ${journal}" EXIT
+    journalctl -t systemd --no-pager --boot > ${journal}
+fi
+
+if is_lineinfile "${REGEXP}" ${journal} ${CITELLUS_ROOT}/var/log/messages ; then
+    echo $"Check Kbase: https://access.redhat.com/solutions/$KCS for more details about error: $REGEXP found in logs" >&2
+    exit ${RC_FAILED}
+else
+    exit ${RC_OKAY}
+fi
