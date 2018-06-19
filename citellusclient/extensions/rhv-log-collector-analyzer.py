4e9981ae3d8c946129904ad931222aa0c5a40ee0
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-06-19 08:37:28 +0200

    Initial support for rhv-log-collector-analizer
    Closes #561
    
    Change-Id: I3414f3922fa75ffc30049ee834b0cc7cae5de931

---
+++ citellusclient/extensions/rhv-log-collector-analyzer.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Extension for processing rhv-log-collector-analizer
+# Author: Pablo Iranzo Gomez (Pablo.Iranzo@gmail.com)
+# Copyright (C) 2017, 2018 Pablo Iranzo Gómez <Pablo.Iranzo@redhat.com>
+
+
+from __future__ import print_function
+
+import os
+
+try:
+    import yaml
+except:
+    pass
+
+try:
+    import citellusclient.shell as citellus
+except:
+    import shell as citellus
+
+# Load i18n settings from citellus
+_ = citellus._
+
+extension = "rhv-log-collector-analyzer"
+pluginsdir = os.path.join(citellus.citellusdir, 'plugins', extension)
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for extension
+    """
+    triggers = ['rhv-log-collector-analyzer']
+    return triggers
+
+
+def listplugins(options=None):
+    """
+    List available plugins
+    :param options: argparse options provided
+    :return: plugin object generator
+    """
+
+    prio = 0
+    if options:
+        try:
+            prio = options.prio
+        except:
+            pass
+
+    yield citellus.findplugins(folders=[pluginsdir], executables=False, fileextension=".txt", extension='rhv-log-collector-analyzer', prio=prio)
+
+
+def get_metadata(plugin):
+    """
+    Gets meadata for plugin
+    :param plugin: plugin object
+    :return: metadata dict for that plugin
+    """
+
+    with open(plugin['plugin'], 'r') as stream:
+        try:
+            doc = (yaml.load(stream))
+        except:
+            doc = ""
+
+    try:
+        description = doc[0]['vars']['metadata']['description']
+    except:
+        description = ""
+
+    metadata = citellus.generic_get_metadata(plugin=plugin)
+    metadata.update({'description': description})
+
+    return metadata
+
+
+def run(plugin):  # do not edit this line
+    """
+    Executes plugin
+    :param plugin: plugin dictionary
+    :return: returncode, out, err
+    """
+
+    rhvlc = citellus.which("rhv-log-collector-analyzer-live")
+    # rhv-log-collector-analyzer-live --json
+    if not rhvlc:
+        return citellus.RC_SKIPPED, '', _('rhv-log-collector-analyzer-live support not found')
+
+    if citellus.CITELLUS_LIVE == 0:
+        # We're running in snapshoot
+        skipped = 1
+    elif citellus.CITELLUS_LIVE == 1:
+        # We're running in Live mode
+        skipped = 0
+    else:
+        # We do not satisfy conditions, exit early
+        skipped = 1
+
+    if skipped == 1:
+        return citellus.RC_SKIPPED, '', _('Plugin does not satisfy conditions for running')
+
+    command = "%s --json" % rhvlc
+
+    # Call exec to run playbook
+    returncode, out, err = citellus.execonshell(filename=command)
+
+    # Do formatting of results and adjust return codes to citellus standards
+    if returncode == 2:
+        returncode = citellus.RC_FAILED
+    elif returncode == 0:
+        returncode = citellus.RC_OKAY
+
+    # Convert stdout to stderr for citellus handling
+    try:
+        err = out
+    except:
+        err = 'Failed to convert output from log-analyzer'
+        returncode = citellus.RC_SKIPPED
+
+    out = ''
+
+    return returncode, out, err
+
+
+def help():  # do not edit this line
+    """
+    Returns help for plugin
+    :return: help text
+    """
+
+    commandtext = _("This extension processes rhv-log-collector-analyzer output")
+    return commandtext
---
+++ citellusclient/hooks/data/9/rhv-log-collector-analyzer-to-individual.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Description: Hook for moving rhv-log-collector-analyzer results to individual tests results
+# Copyright (C) 2018 Pablo Iranzo Gómez <Pablo.Iranzo@redhat.com>
+
+from __future__ import print_function
+
+import os
+import json
+
+try:
+    import citellusclient.shell as citellus
+except:
+    import shell as citellus
+
+# Load i18n settings from citellus
+_ = citellus._
+
+extension = "__file__"
+pluginsdir = os.path.join(citellus.citellusdir, 'plugins', extension)
+
+
+def init():
+    """
+    Initializes module
+    :return: List of triggers for extension
+    """
+    return []
+
+
+def run(data, quiet=False):  # do not edit this line
+    """
+    Executes plugin
+    :param quiet: be more silent on returned information
+    :param data: data to process
+    :return: returncode, out, err
+    """
+
+    # Act on all faraday-exec plugins
+    idstodel = []
+    datatoadd = []
+
+    rhvlcid = citellus.calcid(string='/plugins/rhv-log-collector-analyzer/virtualization/base.txt')
+    # Loop over plugin id's in data
+    for pluginid in data:
+        if data[pluginid]['id'] == rhvlcid and data[pluginid]['result']['rc'] == citellus.RC_OKAY:
+            # Make a copy of dict for working on it
+            try:
+                plugin = json.loads(data[pluginid]['result']['err'])['rhv-log-collector-live']
+            except:
+                plugin = None
+
+            # Fake data until we've the way to run it
+            # plugin = json.load(open('/home/iranzo/DEVEL/citellus/citellus/logcollector2.json', 'r'))['rhv-log-collector-live']
+
+            # Add plugin ID to be removed for resulting data
+            idstodel.append(str(pluginid))
+
+            # Iterate over plugindata items
+            for item in plugin:
+                # Item ID in log-collector is not unique
+                newid = item['id']
+
+                if 'WARNING' in item['message_type']:
+                    returncode = citellus.RC_FAILED
+                else:
+                    returncode = citellus.RC_OKAY
+
+                # Write plugin entry for the individual result
+                newitem = {newid: {'name': 'rhv-log-collector-analyzer: %s' % item['label'],
+                                   'description': item['message'],
+                                   'long_name': item['label'],
+                                   'id': newid,
+                                   'category': '',
+                                   'priority': 400,
+                                   'bugzilla': '',
+                                   'time': 0,
+                                   'subcategory': '',
+                                   'hash': item['filemd5'],
+                                   'result': {'out': '', 'err': "%s" % item['result'], 'rc': returncode},
+                                   'plugin': item['filepath'],
+                                   'backend': 'rhv-log-collector-analyzer',
+                                   'kb': ''}}
+
+                datatoadd.append(newitem)
+
+    # Process id's to remove
+    for id in idstodel:
+        del data[id]
+
+    # Process data to add
+    for item in datatoadd:
+        data.update(item)
+
+    return data
+
+
+def help():  # do not edit this line
+    """
+    Returns help for plugin
+    :return: help text
+    """
+
+    commandtext = _("This hook proceses rhv-log-collector-analyzer results and converts to individual plugins")
+    return commandtext
