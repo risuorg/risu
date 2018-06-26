ff87a7f65fe447f0a6f4659d094e4df426ca7cc2
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-06-26 15:17:31 +0200

    Simplify expiration check
    Change-Id: I5e7a15df97ae878c91d9d99a8cc8cd51859546fc

--- citellusclient/plugins/core/openstack/ceilometer/expiration.sh
+++ citellusclient/plugins/core/openstack/ceilometer/expiration.sh
@@ -36,43 +36,30 @@ MORETHANONCE=$"is listed more than once on file"
 # Check that ceilo polling central is listed (controllers only)
 if is_process "polling-namespaces central" ; then
     if [[ ${RELEASE} -gt 7 ]]; then
-        for string in alarm_history_time_to_live event_time_to_live metering_time_to_live; do
-            # check for string
-            if ! is_lineinfile ^${string} ${FILE};then
-                echo "$string missing on file" >&2
-                RC=${RC_FAILED}
-            elif [[ $(grep -c -e ^${string} ${FILE}) -gt 1 ]]; then
-                echo -n "$string" >&2
-                echo " $MORETHANONCE" >&2
-                RC=${RC_FAILED}
-            else
-                if [[ $(grep -e ^${string} ${FILE}|cut -d "=" -f2) -le 0 ]]; then
-                    echo $"ceilometer.conf setting must be updated:" >&2
-                    RC=${RC_FAILED}
-                    grep -e ^${string} ${FILE} >&2
-                fi
-            fi
-        done
+        strings="alarm_history_time_to_live event_time_to_live metering_time_to_live"
     else
-        for string in time_to_live; do
-            if [[ $(grep -c -e ^${string} ${FILE}) -gt 1 ]]; then
-                echo -n "$string" >&2
-                echo " $MORETHANONCE" >&2
-                RC=${RC_FAILED}
-            else
-                if is_lineinfile ${string} ${FILE}; then
-                    if [[ $(grep -e ^${string} ${FILE}|cut -d "=" -f2|head -1) -le 0 ]]; then
-                        RC=${RC_FAILED}
-                        grep -e ^${string} ${FILE} >&2
-                    fi
-                fi
-            fi
-        done
+        strings="time_to_live"
     fi
+    for string in ${strings}; do
+        # check for string
+        if ! is_lineinfile ^${string} ${FILE};then
+            echo "$string missing on file" >&2
+            RC=${RC_FAILED}
+        elif [[ $(grep -c -e ^${string} ${FILE}) -gt 1 ]]; then
+            echo -n "$string" >&2
+            echo " $MORETHANONCE" >&2
+            RC=${RC_FAILED}
+        else
+            if [[ $(grep -e ^${string} ${FILE}|cut -d "=" -f2) -le 0 ]]; then
+                echo $"ceilometer.conf setting must be updated:" >&2
+                RC=${RC_FAILED}
+                grep -e ^${string} ${FILE} >&2
+            fi
+        fi
+    done
 else
     echo "Works only on controllers" >&2
     RC=${RC_SKIPPED}
 fi
 
 exit ${RC}
-
