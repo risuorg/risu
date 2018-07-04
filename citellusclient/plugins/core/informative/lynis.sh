e4e7c884e46bf189aee57faaf1df54862ecac378
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-07-04 12:05:49 +0200

    Add support for Lynis tool if installed
    Change-Id: I08ae358325f4f7181dfabf5fd42787986d169f73

---
+++ citellusclient/plugins/core/informative/lynis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: Provides output of lynis https://github.com/CISOfy/Lynis
+# description: Reports lynis output https://github.com/CISOfy/Lynis
+# priority: 100
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if ! which lynis >/dev/null 2>&1; then
+    echo "lynis (https://github.com/CISOfy/Lynis) support not found, exitting" >&2
+    exit ${RC_SKIPPED}
+fi
+
+if [[ "x$CITELLUS_LIVE" = "x0" ]];  then
+    echo $"Lynis is not supported for non-live operations" >&2
+    exit ${RC_SKIPPED}
+elif [[ "x$CITELLUS_LIVE" = "x1" ]];  then
+    lynis audit system >&2
+    exit ${RC_INFO}
+fi
+exit ${RC_OKAY}
