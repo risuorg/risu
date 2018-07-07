9dfc75084cb5891c14134eb7249cf65bacc25a61
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-07-07 17:45:05 +0200

    Don't remove old plugins as we might have just filtered them
    Change-Id: I8d0063d2c8ef6b9ff2b578d0b55ef3e4ab287468

--- citellusclient/shell.py
+++ citellusclient/shell.py
@@ -615,25 +615,6 @@ def docitellus(live=False, path=False, plugins=False, lang='en_US', forcerun=Fal
     # Check all sosreports for data for all plugins
     allids = getids(plugins=plugins)
 
-    # Now check in results for id's no longer existing for removal:
-    delete = []
-    for key in iter(results.keys()):
-        if key not in allids:
-            # Plugin ID no longer appears in found plugins.
-            delete.append(key)
-
-    LOG.debug("Removing old plugins from results: %s" % delete)
-
-    for plugid in allids:
-        if plugid not in results and '-' not in plugid:
-            missingplugins.append(plugid)
-
-    LOG.debug("Adding new plugin id's missing to be executed: %s" % missingplugins)
-
-    # Remove old plugins no longer existing from results
-    for key in delete:
-        del results[key]
-
     # Prefill hashes of known plugins for checking same id's with changed hash
     hashes = []
     for plug in plugins:
