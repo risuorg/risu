ee55d48df53d303c6037b296fbe265713962c152
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-07-16 15:32:41 +0200

    Ovirt role detection
    Change-Id: I7efedbbd611038a729b90d7b51207150773deebf

---
+++ citellusclient/plugins/metadata/ovirt-rhv/ovirt-system-role.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: returns the role of the system (hosted-engine-host, rhevm, ovirt-host)
+# description: This plugin is used in various functions. It's just a metadata plugin.
+# priority: 0
+
+# Load common functions
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
+
+ROLE="unknown"
+if is_lineinfile "^vmid" ${CITELLUS_ROOT}/etc/ovirt-hosted-engine/hosted-engine.conf; then
+    ROLE="hosted-engine-host"
+elif is_rpm ovirt-engine >/dev/null 2>&1; then
+    ROLE="rhevm"
+elif is_rpm qemu-kvm-rhev >/dev/null 2>&1; then
+    ROLE="ovirt-host"
+fi
+
+echo ${ROLE} >&2
+exit ${RC_OKAY}
