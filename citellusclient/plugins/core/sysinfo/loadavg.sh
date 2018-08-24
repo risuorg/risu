cc9f143761b6373c73e152c81c6718ba238f5029
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-08-24 23:04:55 +0200

    Add extra sysinfo plugins
    Change-Id: I1663a4a532aa440eb496f31ad51b8f39254e1b58

---
+++ citellusclient/plugins/core/sysinfo/loadavg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: reports uptime and load average
+# description: reports  uptime and load average
+# priority: 100
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if [[ "x$CITELLUS_LIVE" = "x0" ]];  then
+    is_required_file ${CITELLUS_ROOT}/uptime
+    cat ${CITELLUS_ROOT}/uptime >&2
+else
+    uptime >$2
+fi
+
+exit ${RC_OKAY}
---
+++ citellusclient/plugins/core/sysinfo/redhat-release.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: reports running RHEL release
+# description: reports running RHEL release
+# priority: 100
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+is_required_file ${CITELLUS_ROOT}/etc/redhat-release
+cat ${CITELLUS_ROOT}/etc/redhat-release >&2
+
+exit ${RC_OKAY}
