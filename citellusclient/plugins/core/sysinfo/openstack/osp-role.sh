47a226e5270cd7cbf1b0b366a21f805406b00a47
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-08-25 10:41:13 +0200

    Update to have openstack sysinfo profile and filter it out from regular sysinfo
    Change-Id: I046c404c651a52a82d5a545ed0fadd2847390b4d

---
+++ citellusclient/plugins/core/sysinfo/openstack/osp-role.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: reports running OSP role
+# description: reports running OSP rle
+# priority: 100
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+RELEASE=$(discover_osp_version)
+
+if [[ "${RELEASE}" != "0" ]]; then
+    ROLE="unknown"
+    if is_containerized;then
+        ROLE="container-host"
+    elif is_process ironic-conductor;then
+        ROLE="undercloud"
+    elif is_process nova-compute;then
+        ROLE="compute"
+    elif is_process pcsd;then
+        ROLE="controller"
+    elif is_process neutron-server;then
+        # So if neutron-server is running and there's no pcsd, then it's a network node
+        ROLE="network"
+    elif is_process ceilometer-collector;then
+        ROLE="telemetry"
+    fi
+else
+    echo "Couldn't determine OSP release, probably not osp system" >&2
+    exit ${RC_SKIPPED}
+fi
+
+echo ${ROLE} >&2
+exit ${RC_OKAY}
