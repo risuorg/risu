cc35fbdb8a61d6d87ea0f1bd37e8173baf1a3a62
Mikel Olasagasti Uranga <mikel@olasagasti.info>
2018-09-25 17:10:12 +0200

    Detect DigitalOcean
    Change-Id: I12ffa647d1a3678daf6653f7ccbec1d22713b94c

--- citellusclient/common.d/01-virt.sh
+++ citellusclient/common.d/01-virt.sh
@@ -36,6 +36,7 @@ virt_type(){
         is_lineinfile "Product Name: OpenStack Compute" "${FILE}" && echo "OpenStack"
         is_lineinfile "Product Name: Google Compute Engine" "${FILE}" && echo "Google Compute Engine"
         is_lineinfile "Product Name: AHV" "${FILE}" && echo "Nutanix AHV"
+        is_lineinfile "Manufacturer: DigitalOcean" "${FILE}" && echo "DigitalOcean"
         uuid=$(cat "${FILE}"| python ${CITELLUS_BASE}/tools/dmidecode.py| grep UUID |awk '{print $7}' |sed 's/)//')
         amazon=$(cat "${FILE}"| python ${CITELLUS_BASE}/tools/dmidecode.py| grep -c amazon)
         if [[ $(echo $uuid |grep -c ^EC2) -eq 1 ]] || [[ $amazon -gt 0 ]]; then
