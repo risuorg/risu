a7064037ffa83d07711496a5d0e85ad150f430bf
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-09-29 10:22:43 +0200

    Remove extra bracket
    Change-Id: I9d10ab17620252fd970ada9cde9fa6af86d89c4d

--- citellusclient/plugins/core/sysinfo/openstack/osp-role.sh
+++ citellusclient/plugins/core/sysinfo/openstack/osp-role.sh
@@ -21,7 +21,7 @@
 # priority: 100
 
 # Load common functions
-[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+[ -f "${CITELLUS_BASE}/common-functions.sh" ] && . "${CITELLUS_BASE}/common-functions.sh"
 
 RELEASE=$(discover_osp_version)
 
