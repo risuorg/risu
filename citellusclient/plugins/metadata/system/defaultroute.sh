62fa115164a6b8800f97c4c1c4c16419d74c8d87
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-11-05 15:43:25 +0100

    Add route grouping for hosts in same environment
    Change-Id: I020b7c2f12436557198f7ff1084a9812a81abca6

---
+++ citellusclient/plugins/metadata/system/defaultroute.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# long_name: prepares default route metadata
+# description: prepares default route metadata
+
+# Load common functions
+[[ -f "${CITELLUS_BASE}/common-functions.sh" ]] && . "${CITELLUS_BASE}/common-functions.sh"
+
+if [[ ${CITELLUS_LIVE} -eq 0 ]]; then
+    FILE="${CITELLUS_ROOT}/sos_commands/networking/ip_route_show_table_all"
+elif [[ ${CITELLUS_LIVE} -eq 1 ]];then
+    FILE=$(mktemp)
+    trap "rm ${FILE}" EXIT
+    ip route  > ${FILE}
+fi
+
+is_required_file ${FILE}
+
+# Fill metadata 'gateway' to value
+echo "gateway"
+grep ^default ${FILE}|grep -v "::"|cut -d " " -f 3 >&2
+exit ${RC_OKAY}
