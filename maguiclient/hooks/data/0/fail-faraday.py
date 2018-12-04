5917a5e3e73e77612b65f0b29f34fcda14725910
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2018-12-04 22:41:03 +0100

    Implement some logic in loops
    Change-Id: Ic00e4352ee9fb3d52bfa1a27f0b560ef7ecd676f

--- maguiclient/hooks/data/0/fail-faraday.py
+++ maguiclient/hooks/data/0/fail-faraday.py
@@ -41,22 +41,23 @@ def run(data, quiet=False):  # do not edit this line
     # data is a matrix of grouped[plugin][sosreport] and then [text] [out] [err] [rc]
 
     for plugin in data:
-        if 'faraday/' in data[plugin]['plugin']:
-            results = []
-            for sosreport in data[plugin]['sosreport']:
-                results.append(data[plugin]['sosreport'][sosreport]['err'])
-
-            makeitfail = False
-            results = sorted(set(results))
-
-            if len(results) > 1 and 'positive' in data[plugin]['plugin']:
-                makeitfail = True
-            if len(results) < len(data[plugin]['sosreport']) and 'negative' in data[plugin]['plugin']:
-                makeitfail = True
-
-            if makeitfail:
+        if 'plugin' in data[plugin]:
+            if 'faraday/' in data[plugin]['plugin']:
+                results = []
                 for sosreport in data[plugin]['sosreport']:
-                    data[plugin]['sosreport'][sosreport].update({'rc': citellus.RC_FAILED})
+                    results.append(data[plugin]['sosreport'][sosreport]['err'])
+
+                makeitfail = False
+                results = sorted(set(results))
+
+                if len(results) > 1 and 'positive' in data[plugin]['plugin']:
+                    makeitfail = True
+                if len(results) < len(data[plugin]['sosreport']) and 'negative' in data[plugin]['plugin']:
+                    makeitfail = True
+
+                if makeitfail:
+                    for sosreport in data[plugin]['sosreport']:
+                        data[plugin]['sosreport'][sosreport].update({'rc': citellus.RC_FAILED})
 
     return data
 
