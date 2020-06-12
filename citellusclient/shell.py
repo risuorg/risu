23f8fcdbb968f77570990192aa74e1080cc1312a
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-06-12 16:45:49 +0200

    Use absolute path when running citellus to avoid issues if a plugin changes folder
    Change-Id: Idb7a7b76b6a229a0050366e91c87843dd9300471

--- citellusclient/shell.py
+++ citellusclient/shell.py
@@ -546,6 +546,14 @@ def execonshell(filename, timeout=30):
     :param filename: command to run or script name
     :return: returncode, out, err
     """
+
+    if not os.access(filename.split(" ")[0], os.X_OK):
+        returncode = 3
+        out = ""
+        err = "File is not executable"
+
+        return returncode, out, err
+
     try:
         p = subprocess.Popen(
             filename.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE
@@ -567,8 +575,14 @@ def execonshell(filename, timeout=30):
         err = _("Skipped because of execution timeout")
         returncode = int(os.environ["RC_SKIPPED"])
     else:
-        out = out.decode("utf-8").strip()
-        err = err.decode("utf-8").strip()
+        try:
+            out = out.decode("utf-8").strip()
+        except:
+            out = out
+        try:
+            err = err.decode("utf-8").strip()
+        except:
+            err = err
 
     return returncode, out, err
 
@@ -1205,32 +1219,41 @@ def dump_config(options, path=False):
     return json.dumps(differences)
 
 
-def generic_get_metadata(plugin):
+def generic_get_metadata(plugin, comment="#"):
     """
     Gets metadata for plugin
     :param plugin: plugin object
+    :param comment: Character to use as comment in text files
     :return: metadata dict for that plugin
     """
 
-    path = regexpfile(filename=plugin["plugin"], regexp=r"\A# path:")[7:].strip()
+    offset = len(comment) - 1
+
+    path = regexpfile(filename=plugin["plugin"], regexp=r"\A%s path:" % comment)[
+        7 + offset :
+    ].strip()
     path = path.replace("${CITELLUS_ROOT}", "")
 
     metadata = {
         "description": regexpfile(
-            filename=plugin["plugin"], regexp=r"\A# description:"
-        )[14:].strip(),
-        "long_name": regexpfile(filename=plugin["plugin"], regexp=r"\A# long_name:")[
-            12:
-        ].strip(),
-        "bugzilla": regexpfile(filename=plugin["plugin"], regexp=r"\A# bugzilla:")[
-            11:
-        ].strip(),
+            filename=plugin["plugin"], regexp=r"\A%s description:" % comment
+        )[14 + offset :].strip(),
+        "long_name": regexpfile(
+            filename=plugin["plugin"], regexp=r"\A%s long_name:" % comment
+        )[12 + offset :].strip(),
+        "bugzilla": regexpfile(
+            filename=plugin["plugin"], regexp=r"\A%s bugzilla:" % comment
+        )[11 + offset :].strip(),
         "priority": int(
-            regexpfile(filename=plugin["plugin"], regexp=r"\A# priority:")[11:].strip()
+            regexpfile(filename=plugin["plugin"], regexp=r"\A%s priority:" % comment)[
+                11 + offset :
+            ].strip()
             or 0
         ),
         "path": path,
-        "kb": regexpfile(filename=plugin["plugin"], regexp=r"\A# kb:")[5:].strip(),
+        "kb": regexpfile(filename=plugin["plugin"], regexp=r"\A%s kb:" % comment)[
+            5 + offset :
+        ].strip(),
     }
     return metadata
 
@@ -1351,11 +1374,14 @@ def regexpfile(filename=False, regexp=False):
     if not regexp:
         return False
 
-    with open(filename, "r") as f:
-        for line in f:
-            if re.match(regexp, line):
-                # Return earlier if match found
-                return line
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                if re.match(regexp, line):
+                    # Return earlier if match found
+                    return line
+    except:
+        pass
 
     return ""
 
