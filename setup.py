a5e99c4a8237c54849c6598b19f951c92ba54abf
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-08-11 00:00:20 +0200

    Attempt to fix version grabbing

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ import setuptools
 import subprocess
 import time
 
-
 # In python < 2.7.4, a lazy loading of package `pbr` will break
 # setuptools if some other modules registered functions in `atexit`.
 # solution from: http://bugs.python.org/issue15881#msg170215
