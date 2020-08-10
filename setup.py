c1a831325cec825984bd2d44eae22c48b4170a17
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2020-08-10 23:49:24 +0200

    Use GH ref for version

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import setuptools
 import subprocess
 import time
 
+
 # In python < 2.7.4, a lazy loading of package `pbr` will break
 # setuptools if some other modules registered functions in `atexit`.
 # solution from: http://bugs.python.org/issue15881#msg170215
@@ -17,23 +18,36 @@ try:
 except ImportError:
     pass
 
+filename = "setup.cfg"
+regexp = r"\Aversion.*([0-9]+)"
+
+line = ""
+with open(filename, "r") as f:
+    for line in f:
+        if re.match(regexp, line):
+            # Return earlier if match found
+            break
+
+version = line.split("=")[1].strip()
+
 command = "git tag|sort -V|grep -v ^[a-Z]|grep -v 2017|tail -1"
 proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 
 version = out.strip().decode("utf-8")
 
+
 try:
-    travis = os.environ["TRAVIS_JOB_ID"]
+    ghversion = os.environ["RELEASE_VERSION"]
 except:
-    travis = None
+    ghversion = None
 
 strings = time.strftime("%Y,%m,%d,%H,%M,%S")
 t = strings.split(",")
 numbers = [str(x) for x in t]
 
-if travis:
-    os.environ["PBR_VERSION"] = "%s.%s.%s" % (version, travis, "".join(numbers))
+if ghversion:
+    os.environ["PBR_VERSION"] = "%s.%s.%s" % (version, ghversion, "".join(numbers))
 else:
     os.environ["PBR_VERSION"] = "%s.%s.%s" % (version, 0, "".join(numbers))
 
