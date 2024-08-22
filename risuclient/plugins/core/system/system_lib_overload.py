8948752ed822e4eec0176b3fb55540104660129e
iranzo <iranzo@users.noreply.github.com>
2024-08-22 14:01:25 +0000

    [skip ci] Autoupdate (C) on script change
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- risuclient/plugins/core/system/system_lib_overload.py
+++ risuclient/plugins/core/system/system_lib_overload.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-# Copyright (C) 2022, 2023 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
+# Copyright (C) 2022-2024 Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
